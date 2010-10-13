f5e0f0404abd14fe5b6ce0f3c1f131fd8204d6cf
Ralph Giles <giles@xiph.org>
2010-10-13 09:01:05 -0700

    Add a GNU GPLv2+ copyright header to each source file.
    Based on a patch by Matt Lee.

--- src/x86/x86int.h
+++ src/x86/x86int.h
@@ -1,3 +1,24 @@
+/*
+    Daala video codec
+    Copyright (C) 2010 Timothy B. Terriberry and Daala project contributors
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+
 #if !defined(_x86_x86int_H)
 # define _x86_x86int_H (1)
 # include "../state.h"
