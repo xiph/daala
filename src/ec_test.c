4555db7464a7c4c5be040a86f65ccec01151b39e
Ralph Giles <giles@xiph.org>
2010-10-13 09:32:56 -0700

    Update the copyright header with appropriate date ranges.
    We want to show the span of development for the various files,
    so we use a range of years in the copyright statement. Since
    that makes the line too long, we shorten the copyright holder
    to just 'Daala project contributors' which is currently defined
    to be juse 'Timothy B. Terriberry' in the file AUTHORS.

--- src/ec_test.c
+++ src/ec_test.c
@@ -1,6 +1,6 @@
 /*
     Daala video codec
-    Copyright (C) 2010 Timothy B. Terriberry and Daala project contributors
+    Copyright (C) 2001-2010 Daala project contributors
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
