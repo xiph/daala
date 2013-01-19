5a16a5fb852e86b27c54fceb117addba6452f0f5
Jean-Marc Valin <jmvalin@jmvalin.ca>
2013-01-19 06:10:37 -0500

    New log-based psy model for block switching

--- src/switch_decision.c
+++ src/switch_decision.c
@@ -13,9 +13,9 @@
 #define CG16 (9.81*2/6)
 #define CG32 (9.93*2/6)
 
-#define OFF8   (1)
-#define OFF16  (2)
-#define OFF32  (3)
+#define OFF8   (2)
+#define OFF16  (4)
+#define OFF32  (4)
 
 #define COUNT8   (3+2*OFF8)
 #define COUNT16  (7+2*OFF16)
@@ -76,9 +76,7 @@ void od_switch_process_superblock(od_switch_decision_state *st, const unsigned c
 }
 
 
-/* Fudge factor giving more importance to NMR compared to coding gain.
-   Partially compensates for the fact that edges have a lower coding gain */
-#define fudge 1.0
+#define PSY_LAMBDA 1.0
 
 int switch_decision(const unsigned char *img, int w, int h, int stride)
 {
@@ -125,7 +123,7 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       sum_x=Sx[i][j]+Sx[i][j+1]+Sx[i+1][j]+Sx[i+1][j+1];
       sum_xx=Sxx[i][j]+Sxx[i][j+1]+Sxx[i+1][j]+Sxx[i+1][j+1];
       var[i][j]=(sum_xx-(SQUARE(sum_x)>>4))>>5;
-      var_floor = 2+(sum_x>>9);
+      var_floor = 4+(sum_x>>8);
       if (var[i][j]<var_floor)var[i][j]=var_floor;
       /*printf("%d ", var[i][j]);*/
       var_1[i][j] = 16384/var[i][j];
@@ -138,13 +136,21 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       int k,m;
       int sum_var=0;
       int sum_var_1=0;
+      float psy=0;
+      float noise;
       for(k=0;k<3;k++){
         for(m=0;m<3;m++){
           sum_var+=var[2*i-1+k][2*j-1+m];
-          sum_var_1+=var_1[2*i-1+k][2*j-1+m];
         }
       }
-      nmr4[i][j] = (float)sum_var*(float)sum_var_1/81.;
+      noise = sum_var/(3*3);
+      for(k=0;k<3;k++){
+        for(m=0;m<3;m++){
+          psy += log2(1+noise*var_1[2*i-1+k][2*j-1+m]/16384.);
+        }
+      }
+      psy /= (3*3);
+      nmr4[i][j] = psy;
       /*printf("%f ", nmr4[i][j]);*/
     }
     /*printf("\n");*/
@@ -157,16 +163,25 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       int sum_var_1=0;
       float nmr4_avg;
       float cgl, cgs;
+      float noise;
+      float psy;
       for(k=0;k<COUNT8;k++){
         for(m=0;m<COUNT8;m++){
-          sum_var  +=var  [4*i-OFF8+k][4*j-OFF8+m];
-          sum_var_1+=var_1[4*i-OFF8+k][4*j-OFF8+m];
+          sum_var  +=var[4*i-OFF8+k][4*j-OFF8+m];
         }
       }
-      nmr8[i][j] = (float)sum_var*(float)sum_var_1/(COUNT8*COUNT8*COUNT8*COUNT8);
+      noise = sum_var/(COUNT8*COUNT8);
+      psy=0;
+      for(k=0;k<COUNT8;k++){
+        for(m=0;m<COUNT8;m++){
+          psy += log2(1+noise*var_1[4*i-OFF8+k][4*j-OFF8+m]/16384.);
+        }
+      }
+      psy /= (COUNT8*COUNT8);
+      nmr8[i][j] = psy;
       nmr4_avg = .25f*(nmr4[2*i][2*j]+nmr4[2*i][2*j+1]+nmr4[2*i+1][2*j]+nmr4[2*i+1][2*j+1]);
-      cgs = CG4 - fudge*.5*log2(nmr4_avg);
-      cgl = CG8 - fudge*.5*log2(nmr8[i][j]);
+      cgs = CG4 - PSY_LAMBDA*(nmr4_avg);
+      cgl = CG8 - PSY_LAMBDA*(nmr8[i][j]);
       if (cgl>=cgs)
       {
         dec8[i][j] = 1;
@@ -188,24 +203,28 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       int sum_var_1=0;
       float nmr8_avg;
       float cgl,cgs;
-      float bias=0;
-
-      /* Bias against 16x16 is there's a block that's better with 4x4 than 8x8 */
-      if (!(dec8[2*i][2*j]&&dec8[2*i][2*j+1]&&dec8[2*i+1][2*j]&&dec8[2*i+1][2*j+1]))
-        bias=-.00;
+      float noise;
+      float psy;
       for(k=0;k<COUNT16;k++){
         for(m=0;m<COUNT16;m++){
-          sum_var  +=var  [8*i-OFF16+k][8*j-OFF16+m];
-          sum_var_1+=var_1[8*i-OFF16+k][8*j-OFF16+m];
+          sum_var+=var[8*i-OFF16+k][8*j-OFF16+m];
         }
       }
-      nmr16[i][j] = (float)sum_var*(float)sum_var_1/(COUNT16*COUNT16*COUNT16*COUNT16);
+      noise = sum_var/(float)(COUNT16*COUNT16);
+      psy=0;
+      for(k=0;k<COUNT16;k++){
+        for(m=0;m<COUNT16;m++){
+          psy += log2(1+noise*var_1[8*i-OFF16+k][8*j-OFF16+m]/16384.);
+        }
+      }
+      psy /= (COUNT16*COUNT16);
+      nmr16[i][j] = psy;
       nmr8_avg = .25f*(nmr8[2*i][2*j]+nmr8[2*i][2*j+1]+nmr8[2*i+1][2*j]+nmr8[2*i+1][2*j+1]);
-      //if (nmr8_avg>nmr16[i][j])nmr8_avg=nmr16[i][j];
       cg16[i][j] = .25*(cg8[2*i][2*j] + cg8[2*i][2*j+1] + cg8[2*i+1][2*j] + cg8[2*i+1][2*j+1]);
-      cgs = cg16[i][j] - fudge*.5*log2(nmr8_avg);
-      cgl = CG16 - fudge*.5*log2(nmr16[i][j]);
-      if (cgl+bias>=cgs)
+      cgs = cg16[i][j] - PSY_LAMBDA*(nmr8_avg);
+      cgl = CG16 - PSY_LAMBDA*(nmr16[i][j]);
+      /*printf("%f ", psy);*/
+      if (cgl>=cgs)
       {
         dec8[2*i][2*j] = 2;
         dec8[2*i][2*j+1] = 2;
@@ -215,7 +234,6 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       } else {
         nmr16[i][j] = nmr8_avg;
       }
-      /*printf("%f ", nmr16[i][j]);*/
     }
     /*printf("\n");*/
   }
@@ -228,24 +246,27 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       int sum_var_1=0;
       float nmr16_avg;
       float cgl,cgs;
-      float bias = 0;
-
-      /* Bias against 32x32 is there's a block that's better with 4x4 or 8x8 than 16x16 */
-      if (!(dec8[4*i][4*j]==2&&dec8[4*i][4*j+2]==2&&dec8[4*i+2][4*j]==2&&dec8[4*i+2][4*j+2]==2))
-        bias=-.0;
+      float noise, psy;
       for(k=0;k<COUNT32;k++){
         for(m=0;m<COUNT32;m++){
-          sum_var  +=var  [16*i-OFF32+k][16*j-OFF32+m];
-          sum_var_1+=var_1[16*i-OFF32+k][16*j-OFF32+m];
+          sum_var  +=var[16*i-OFF32+k][16*j-OFF32+m];
         }
       }
-      nmr32[i][j] = (float)sum_var*(float)sum_var_1/(COUNT32*COUNT32*COUNT32*COUNT32);
+      noise = sum_var/(float)(COUNT32*COUNT32);
+      psy=0;
+      for(k=0;k<COUNT32;k++){
+        for(m=0;m<COUNT32;m++){
+          psy += log2(1.+noise*var_1[16*i-OFF32+k][16*j-OFF32+m]/16384.);
+        }
+      }
+      psy /= (COUNT32*COUNT32);
+      nmr32[i][j] = psy;
       nmr16_avg = .25f*(nmr16[2*i][2*j]+nmr16[2*i][2*j+1]+nmr16[2*i+1][2*j]+nmr16[2*i+1][2*j+1]);
-      //if (nmr16_avg>nmr32[i][j])nmr16_avg=nmr32[i][j];
       cg32[i][j] = .25*(cg16[2*i][2*j] + cg16[2*i][2*j+1] + cg16[2*i+1][2*j] + cg16[2*i+1][2*j+1]);
-      cgs = cg32[i][j] - fudge*.5*log2(nmr16_avg);
-      cgl = CG32 - fudge*.5*log2(nmr32[i][j]);
-      if (cgl+bias>=cgs)
+      cgs = cg32[i][j] - PSY_LAMBDA*(nmr16_avg);
+      cgl = CG32 - PSY_LAMBDA*(nmr32[i][j]);
+      /*printf("%f ", nmr32[i][j]);*/
+      if (cgl>=cgs)
       {
         for(k=0;k<4;k++){
           for(m=0;m<4;m++){
@@ -256,12 +277,11 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       } else {
         nmr32[i][j] = nmr16_avg;
       }
-      /*printf("%f ", nmr16[i][j]);*/
     }
     /*printf("\n");*/
   }
 #endif
-#if 0
+#if 1
   for(i=4;i<h8-4;i++){
     for(j=4;j<w8-4;j++){
       printf("%d ", dec8[i][j]);
@@ -269,7 +289,7 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
     printf("\n");
   }
 #endif
-#if 1
+#if 0
   fprintf(stderr, "size : %dx%d\n", (w<<1), (h<<1));
   for(i=0;i<(h<<1);i++){
     for(j=0;j<1296;j++){
