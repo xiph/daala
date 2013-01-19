776d2e7c790f65205bb785eb4139446dcd4b7fa0
Jean-Marc Valin <jmvalin@jmvalin.ca>
2013-01-19 01:16:24 -0500

    Make it easy to change the size of the switching decision overlap

--- src/switch_decision.c
+++ src/switch_decision.c
@@ -13,6 +13,14 @@
 #define CG16 (9.81*2/6)
 #define CG32 (9.93*2/6)
 
+#define OFF8   (1)
+#define OFF16  (2)
+#define OFF32  (3)
+
+#define COUNT8   (3+2*OFF8)
+#define COUNT16  (7+2*OFF16)
+#define COUNT32 (15+2*OFF32)
+
 void od_switch_init(od_switch_decision_state *st, int w)
 {
   st->vstride = (w/2)+32;
@@ -58,10 +66,10 @@ void od_switch_process_superblock(od_switch_decision_state *st, const unsigned c
       ogg_int16_t var_floor;
       sum_x=Sx[i][j]+Sx[i][j+1]+Sx[i+1][j]+Sx[i+1][j+1];
       sum_xx=Sxx[i][j]+Sxx[i][j+1]+Sxx[i+1][j]+Sxx[i+1][j+1];
-      /*var[i][j]=(sum_xx-(SQUARE(sum_x)>>4))>>5;
+      var[voffset+i*vs+j]=(sum_xx-(SQUARE(sum_x)>>4))>>5;
       var_floor = 4+(sum_x>>6);
-      if (var[i][j]<var_floor)var[i][j]=var_floor;
-      var_1[i][j] = 16384/var[i][j];*/
+      if (var[voffset+i*vs+j]<var_floor)var[voffset+i*vs+j]=var_floor;
+      /*var_1[i][j] = 16384/var[i][j];*/
       st->var[voffset+i*vs+j] = 0;
     }
   }
@@ -130,15 +138,12 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       int k,m;
       int sum_var=0;
       int sum_var_1=0;
-      float maxvar = 0;
       for(k=0;k<3;k++){
         for(m=0;m<3;m++){
-          if (var[2*i-1+k][2*j-1+m]>maxvar)maxvar=var[2*i-1+k][2*j-1+m];
           sum_var+=var[2*i-1+k][2*j-1+m];
           sum_var_1+=var_1[2*i-1+k][2*j-1+m];
         }
       }
-      //sum_var=sqrt(sum_var*maxvar*9);
       nmr4[i][j] = (float)sum_var*(float)sum_var_1/81.;
       /*printf("%f ", nmr4[i][j]);*/
     }
@@ -152,18 +157,14 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       int sum_var_1=0;
       float nmr4_avg;
       float cgl, cgs;
-      float maxvar = 0;
-      for(k=0;k<7;k++){
-        for(m=0;m<7;m++){
-          if (var[4*i-2+k][4*j-2+m]>maxvar)maxvar=var[4*i-2+k][4*j-2+m];
-          sum_var+=var[4*i-2+k][4*j-2+m];
-          sum_var_1+=var_1[4*i-2+k][4*j-2+m];
+      for(k=0;k<COUNT8;k++){
+        for(m=0;m<COUNT8;m++){
+          sum_var  +=var  [4*i-OFF8+k][4*j-OFF8+m];
+          sum_var_1+=var_1[4*i-OFF8+k][4*j-OFF8+m];
         }
       }
-      //sum_var=sqrt(sum_var*maxvar*7*7);
-      nmr8[i][j] = (float)sum_var*(float)sum_var_1/(7*7*7*7);
+      nmr8[i][j] = (float)sum_var*(float)sum_var_1/(COUNT8*COUNT8*COUNT8*COUNT8);
       nmr4_avg = .25f*(nmr4[2*i][2*j]+nmr4[2*i][2*j+1]+nmr4[2*i+1][2*j]+nmr4[2*i+1][2*j+1]);
-      //if (nmr4_avg>nmr8[i][j])nmr4_avg=nmr8[i][j];
       cgs = CG4 - fudge*.5*log2(nmr4_avg);
       cgl = CG8 - fudge*.5*log2(nmr8[i][j]);
       if (cgl>=cgs)
@@ -188,20 +189,17 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       float nmr8_avg;
       float cgl,cgs;
       float bias=0;
-      float maxvar = 0;
 
       /* Bias against 16x16 is there's a block that's better with 4x4 than 8x8 */
       if (!(dec8[2*i][2*j]&&dec8[2*i][2*j+1]&&dec8[2*i+1][2*j]&&dec8[2*i+1][2*j+1]))
         bias=-.00;
-      for(k=0;k<15;k++){
-        for(m=0;m<15;m++){
-          if (var[8*i-4+k][8*j-4+m]>maxvar)maxvar=var[8*i-4+k][8*j-4+m];
-          sum_var+=var[8*i-4+k][8*j-4+m];
-          sum_var_1+=var_1[8*i-4+k][8*j-4+m];
+      for(k=0;k<COUNT16;k++){
+        for(m=0;m<COUNT16;m++){
+          sum_var  +=var  [8*i-OFF16+k][8*j-OFF16+m];
+          sum_var_1+=var_1[8*i-OFF16+k][8*j-OFF16+m];
         }
       }
-      //sum_var=sqrt(sum_var*maxvar*15*15);
-      nmr16[i][j] = (float)sum_var*(float)sum_var_1/(15*15*15*15);
+      nmr16[i][j] = (float)sum_var*(float)sum_var_1/(COUNT16*COUNT16*COUNT16*COUNT16);
       nmr8_avg = .25f*(nmr8[2*i][2*j]+nmr8[2*i][2*j+1]+nmr8[2*i+1][2*j]+nmr8[2*i+1][2*j+1]);
       //if (nmr8_avg>nmr16[i][j])nmr8_avg=nmr16[i][j];
       cg16[i][j] = .25*(cg8[2*i][2*j] + cg8[2*i][2*j+1] + cg8[2*i+1][2*j] + cg8[2*i+1][2*j+1]);
@@ -231,20 +229,17 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
       float nmr16_avg;
       float cgl,cgs;
       float bias = 0;
-      float maxvar = 0;
 
       /* Bias against 32x32 is there's a block that's better with 4x4 or 8x8 than 16x16 */
       if (!(dec8[4*i][4*j]==2&&dec8[4*i][4*j+2]==2&&dec8[4*i+2][4*j]==2&&dec8[4*i+2][4*j+2]==2))
         bias=-.0;
-      for(k=0;k<31;k++){
-        for(m=0;m<31;m++){
-          if (var[16*i-8+k][16*j-8+m]>maxvar)maxvar=var[16*i-8+k][16*j-8+m];
-          sum_var+=var[16*i-8+k][16*j-8+m];
-          sum_var_1+=var_1[16*i-8+k][16*j-8+m];
+      for(k=0;k<COUNT32;k++){
+        for(m=0;m<COUNT32;m++){
+          sum_var  +=var  [16*i-OFF32+k][16*j-OFF32+m];
+          sum_var_1+=var_1[16*i-OFF32+k][16*j-OFF32+m];
         }
       }
-      //sum_var=sqrt(sum_var*maxvar*31*31);
-      nmr32[i][j] = (float)sum_var*(float)sum_var_1/(31*31*31*31);
+      nmr32[i][j] = (float)sum_var*(float)sum_var_1/(COUNT32*COUNT32*COUNT32*COUNT32);
       nmr16_avg = .25f*(nmr16[2*i][2*j]+nmr16[2*i][2*j+1]+nmr16[2*i+1][2*j]+nmr16[2*i+1][2*j+1]);
       //if (nmr16_avg>nmr32[i][j])nmr16_avg=nmr32[i][j];
       cg32[i][j] = .25*(cg16[2*i][2*j] + cg16[2*i][2*j+1] + cg16[2*i+1][2*j] + cg16[2*i+1][2*j+1]);
@@ -266,12 +261,20 @@ int switch_decision(const unsigned char *img, int w, int h, int stride)
     /*printf("\n");*/
   }
 #endif
-#if 1
+#if 0
   for(i=4;i<h8-4;i++){
     for(j=4;j<w8-4;j++){
       printf("%d ", dec8[i][j]);
     }
     printf("\n");
+  }
+#endif
+#if 1
+  fprintf(stderr, "size : %dx%d\n", (w<<1), (h<<1));
+  for(i=0;i<(h<<1);i++){
+    for(j=0;j<1296;j++){
+      putc(dec8[i>>3][j>>3], stdout);
+    }
   }
 #endif
   return 0;
