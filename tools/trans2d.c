6bf3800cb20fdc45282c013bec5a832199b22ff8
Nathan E. Egge <negge@dgql.org>
2013-04-18 22:55:50 -0400

    Adding exhaustive R=t,D=t search for B_SZ=4 and B_SZ=8

--- tools/trans2d.c
+++ tools/trans2d.c
@@ -10,6 +10,8 @@
 #define CG_SEARCH (0)
 #define PRINT_COV (0)
 
+#define RAMP_DYADIC (0)
+
 #define USE_FILES   (1)
 #define USE_AR95    (0)
 #define USE_SUBSET1 (0)
@@ -19,7 +21,7 @@ static void coding_gain_search(const double _r[2*B_SZ*2*B_SZ],const int *_f){
 #if CG_SEARCH
 # if B_SZ==4
   {
-    int    filt[4];
+    int    f[4];
     int    p0;
     int    q0;
     int    s0;
@@ -27,18 +29,122 @@ static void coding_gain_search(const double _r[2*B_SZ*2*B_SZ],const int *_f){
     double cg;
     double best_cg;
     best_cg=0;
-    for(p0=-(1<<NE_BITS);p0<=(1<<NE_BITS);p0++){
-      filt[2]=p0;
-      for(q0=(1<<NE_BITS);q0>=-(1<<NE_BITS);q0--){
-        filt[3]=q0;
-        for(s0=(1<<NE_BITS);s0<=2*(1<<NE_BITS);s0++){
-          filt[0]=s0;
-          for(s1=(1<<NE_BITS);s1<=2*(1<<NE_BITS);s1++){
-            filt[1]=s1;
-            cg=coding_gain_2d_collapsed(_r,filt);
+#if RAMP_DYADIC
+    for(q0=(1<<NE_BITS);q0>=-(1<<NE_BITS);q0--){
+      f[3]=q0;
+      /* S0 = 4/1*(1-q0/64)
+       * S0 >= 1 -> 64-q0 >= 16
+       */
+      s0=(1<<NE_BITS)-q0;
+      if(s0>=(1<<NE_BITS-2)){
+        f[0]=s0*4;
+        for(p0=-(1<<NE_BITS);p0<=(1<<NE_BITS);p0++){
+          f[2]=p0;
+          /* S1 = 4/3*(1-(1-q0/64)*p0/64)
+           * S1 >= 1   -> 64^2-(64-q0)*p0 >= 64*48
+           * S1 = x/64 -> 64^2-(64-q0)*p0 = 0 MOD 48
+           */
+          s1=(1<<2*NE_BITS)-s0*p0;
+          if(s1>=(1<<NE_BITS)*(3<<NE_BITS-2)&&s1%(3<<NE_BITS-2)==0){
+            f[1]=s1/48;
+            cg=coding_gain_2d_collapsed(_r,f);
             if(cg>best_cg){
               best_cg=cg;
-              fprintf(stdout,"%i %i %i %i %G\n",p0,q0,s0,s1,cg);
+              printf("%i %i %i %i %G\n",f[2],f[3],f[0],f[1],cg);
+            }
+          }
+        }
+      }
+    }
+#else
+    for(p0=-(1<<NE_BITS);p0<=(1<<NE_BITS);p0++){
+      f[2]=p0;
+      for(q0=(1<<NE_BITS);q0>=-(1<<NE_BITS);q0--){
+        f[3]=q0;
+        for(s0=(1<<NE_BITS);s0<=2*(1<<NE_BITS);s0++){
+          f[0]=s0;
+          for(s1=(1<<NE_BITS);s1<=2*(1<<NE_BITS);s1++){
+            f[1]=s1;
+            cg=coding_gain_2d_collapsed(_r,f);
+            if(cg>best_cg){
+              best_cg=cg;
+              printf("%i %i %i %i %G\n",f[2],f[3],f[0],f[1],cg);
+            }
+          }
+        }
+      }
+    }
+#endif
+  }
+# elif B_SZ==8 && RAMP_DYADIC
+  {
+    int    f[10];
+    int    p0;
+    int    p1;
+    int    p2;
+    int    q0;
+    int    q1;
+    int    q2;
+    int    s0;
+    int    s1;
+    int    s2;
+    int    s3;
+    int    t1;
+    int    t2;
+    double cg;
+    double best_cg;
+    for(q0=(1<<NE_BITS);q0>=-(1<<NE_BITS);q0--){
+      f[7]=q0;
+      /* S0 = 8/1*(1-q0/64)
+       * S0 >= 1 -> 64-q0 >= 8
+       */
+      s0=(1<<NE_BITS)-q0;
+      if(s0>=(1<<NE_BITS-3)){
+        f[0]=s0*8;
+        for(p0=-(1<<NE_BITS);p0<=(1<<NE_BITS);p0++){
+          f[4]=p0;
+          for(q1=(1<<NE_BITS);q1>=-(1<<NE_BITS);q1--){
+            f[8]=q1;
+            /* S1 = 8/3*((1-q1/64)-(1-q0/64)*p0/64)
+             * S1 >= 1   -> 64*t1-t0*p0 >= 64*24
+             * S1 = x/64 -> 64*t1-t0*p0 = 0 MOD 24
+             */
+            t1=(1<<NE_BITS)-q1;
+            s1=(1<<NE_BITS)*t1-s0*p0;
+            if(s1>=(1<<NE_BITS)*(3<<NE_BITS-3)&&s1%(3<<NE_BITS-3)==0){
+              f[1]=s1/24;
+              for(p1=-(1<<NE_BITS);p1<=(1<<NE_BITS);p1++){
+                f[5]=p1;
+                for(q2=(1<<NE_BITS);q2>=-(1<<NE_BITS);q2--){
+                  f[9]=q2;
+                  /* S2 = 8/5*((1-q2/64)-(1-q1/64)*p1/64)
+                   * S2 >= 1   -> 64*t2-t1*p1) >= 64*40
+                   * S2 = x/64 -> 64*t2-t1*p1 = 0 MOD 40
+                   */
+                  t2=(1<<NE_BITS)-q2;
+                  s2=(1<<NE_BITS)*t2-t1*p1;
+                  if(s2>=(1<<NE_BITS)*(5<<NE_BITS-3)&&s2%(5<<NE_BITS-3)==0){
+                    f[2]=s2/40;
+                    for(p2=-(1<<NE_BITS);p2<=(1<<NE_BITS);p2++){
+                      f[6]=p2;
+                      /* S3 = 8/7*(1-(1-q2/64)*p2/64)
+                       * S3 >= 1   -> 64^2-t2*p2 >= 64*56
+                       * S3 = x/64 -> 64^2-t2*p2 = 0 MOD 56
+                       */
+                      s3=(1<<2*NE_BITS)-t2*p2;
+                      if(s3>=(1<<NE_BITS)*(7<<NE_BITS-3)&&s3%(7<<NE_BITS-3)==0){
+                        f[3]=s3/56;
+                        cg=coding_gain_2d_collapsed(_r,f);
+                        if(cg>best_cg){
+                          best_cg=cg;
+                          printf("%i %i %i %i %i %i %i %i %i %i %-24.18G\n",
+                          f[4],f[5],f[6],f[7],f[8],f[9],f[0],f[1],f[2],f[3],cg);
+                        }
+                      }
+                    }
+                  }
+                }
+              }
             }
           }
         }
