f7e6ca1e245b8817dad643deeae689502b91a37a
Timothy B. Terriberry <tterribe@xiph.org>
2013-05-26 16:05:11 -0700

    Reformatting code to new style: tf.[ch]

--- src/tf.c
+++ src/tf.c
@@ -11,7 +11,7 @@ modification, are permitted provided that the following conditions are met:
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
@@ -21,161 +21,162 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
 #include "tf.h"
 
 /*Increase horizontal frequency resolution of an entire block and return the LF
    half.*/
-void od_tf_up_h_lp(od_coeff *_dst,int _dstride,
- const od_coeff *_src,int _sstride,int _dx,int _n){
+void od_tf_up_h_lp(od_coeff *dst, int dstride,
+ const od_coeff *src, int sstride, int dx, int n) {
   int x;
   int y;
-  for(y=0;y<_n;y++){
-    for(x=0;x<_n>>1;x++){
+  for (y = 0; y <n; y++) {
+    for (x = 0; x < n >> 1; x++) {
       od_coeff ll;
       od_coeff lh;
-      int      hswap;
-      ll=_src[y*_sstride+x];
-      lh=_src[y*_sstride+x+_dx];
-      lh=ll-lh;
-      ll-=OD_DCT_RSHIFT(lh,1);
-      hswap=x&1;
-      _dst[y*_dstride+2*x+hswap]=ll;
-      _dst[y*_dstride+2*x+1-hswap]=lh;
+      int hswap;
+      ll = src[y*sstride + x];
+      lh = src[y*sstride + x + dx];
+      lh = ll - lh;
+      ll -= OD_DCT_RSHIFT(lh, 1);
+      hswap = x & 1;
+      dst[y*dstride + 2*x + hswap] = ll;
+      dst[y*dstride + 2*x + 1 - hswap] = lh;
     }
   }
 }
 
 /*Increase vertical frequency resolution of an entire block and return the LF
    half.*/
-void od_tf_up_v_lp(od_coeff *_dst,int _dstride,
- const od_coeff *_src,int _sstride,int _dy,int _n){
+void od_tf_up_v_lp(od_coeff *dst, int dstride,
+ const od_coeff *src, int sstride, int dy, int n) {
   int x;
   int y;
-  for(y=0;y<_n>>1;y++){
+  for (y = 0; y < n >> 1; y++) {
     int vswap;
-    vswap=y&1;
-    for(x=0;x<_n;x++){
+    vswap = y & 1;
+    for (x = 0; x < n; x++) {
       od_coeff ll;
       od_coeff hl;
-      ll=_src[y*_sstride+x];
-      hl=_src[(y+_dy)*_sstride+x];
-      hl=ll-hl;
-      ll-=OD_DCT_RSHIFT(hl,1);
-      _dst[(2*y+vswap)*_dstride+x]=ll;
-      _dst[(2*y+1-vswap)*_dstride+x]=hl;
+      ll = src[y*sstride + x];
+      hl = src[(y + dy)*sstride + x];
+      hl = ll - hl;
+      ll -= OD_DCT_RSHIFT(hl, 1);
+      dst[(2*y + vswap)*dstride + x] = ll;
+      dst[(2*y + 1 - vswap)*dstride + x] = hl;
     }
   }
 }
 
 /*Increase horizontal and vertical frequency resolution of an entire block and
    return the LF quarter.*/
-void od_tf_up_hv_lp(od_coeff *_dst,int _dstride,
- const od_coeff *_src,int _sstride,int _dx,int _dy,int _n){
+void od_tf_up_hv_lp(od_coeff *dst, int dstride,
+ const od_coeff *src, int sstride, int dx, int dy, int n) {
   int x;
   int y;
-  for(y=0;y<_n>>1;y++){
+  for (y = 0; y < n >> 1; y++) {
     int vswap;
-    vswap=y&1;
-    for(x=0;x<_n>>1;x++){
+    vswap = y & 1;
+    for (x = 0; x < n >> 1; x++) {
       od_coeff ll;
       od_coeff lh;
       od_coeff hl;
       od_coeff hh;
       od_coeff lhmhl_2;
-      int      hswap;
-      ll=_src[y*_sstride+x];
-      lh=_src[y*_sstride+x+_dx];
-      hl=_src[(y+_dy)*_sstride+x];
-      hh=_src[(y+_dy)*_sstride+x+_dx];
-      hl=ll-hl;
-      lh+=hh;
-      lhmhl_2=OD_DCT_RSHIFT(lh-hl,1);
-      ll+=lhmhl_2;
-      hh-=lhmhl_2;
-      lh=ll-lh;
-      hl-=hh;
-      hswap=x&1;
-      _dst[(2*y+vswap)*_dstride+2*x+hswap]=ll;
-      _dst[(2*y+vswap)*_dstride+2*x+1-hswap]=lh;
-      _dst[(2*y+1-vswap)*_dstride+2*x+hswap]=hl;
-      _dst[(2*y+1-vswap)*_dstride+2*x+1-hswap]=hh;
+      int hswap;
+      ll = src[y*sstride + x];
+      lh = src[y*sstride + x + dx];
+      hl = src[(y + dy)*sstride + x];
+      hh = src[(y + dy)*sstride + x + dx];
+      hl = ll - hl;
+      lh += hh;
+      lhmhl_2 = OD_DCT_RSHIFT(lh - hl, 1);
+      ll += lhmhl_2;
+      hh -= lhmhl_2;
+      lh = ll - lh;
+      hl -= hh;
+      hswap = x & 1;
+      dst[(2*y + vswap)*dstride + 2*x + hswap] = ll;
+      dst[(2*y + vswap)*dstride + 2*x + 1 - hswap] = lh;
+      dst[(2*y + 1 - vswap)*dstride + 2*x + hswap] = hl;
+      dst[(2*y + 1 - vswap)*dstride + 2*x + 1 - hswap] = hh;
     }
   }
 }
 
 /*Increase horizontal and vertical frequency resolution of a 2x2 group of
    blocks, combining them into a single block.*/
-void od_tf_up_hv(od_coeff *_dst,int _dstride,
- const od_coeff *_src,int _sstride,int _n){
+void od_tf_up_hv(od_coeff *dst, int dstride,
+ const od_coeff *src, int sstride, int n) {
   int x;
   int y;
-  for(y=0;y<_n;y++){
+  for (y = 0; y < n; y++) {
     int vswap;
-    vswap=y&1;
-    for(x=0;x<_n;x++){
+    vswap = y & 1;
+    for (x = 0; x < n; x++) {
       od_coeff ll;
       od_coeff lh;
       od_coeff hl;
       od_coeff hh;
       od_coeff lhmhl_2;
-      int      hswap;
-      ll=_src[y*_sstride+x];
-      lh=_src[y*_sstride+x+_n];
-      hl=_src[(y+_n)*_sstride+x];
-      hh=_src[(y+_n)*_sstride+x+_n];
+      int hswap;
+      ll = src[y*sstride + x];
+      lh = src[y*sstride + x + n];
+      hl = src[(y + n)*sstride + x];
+      hh = src[(y + n)*sstride + x + n];
       /*This kernel is identical to that of od_tf_down_hv with the roles of
          hl and lh swapped.*/
-      hl=ll-hl;
-      lh+=hh;
-      lhmhl_2=OD_DCT_RSHIFT(lh-hl,1);
-      ll+=lhmhl_2;
-      hh-=lhmhl_2;
-      lh=ll-lh;
-      hl-=hh;
-      hswap=x&1;
-      _dst[(2*y+vswap)*_dstride+2*x+hswap]=ll;
-      _dst[(2*y+vswap)*_dstride+2*x+1-hswap]=lh;
-      _dst[(2*y+1-vswap)*_dstride+2*x+hswap]=hl;
-      _dst[(2*y+1-vswap)*_dstride+2*x+1-hswap]=hh;
+      hl = ll - hl;
+      lh += hh;
+      lhmhl_2 = OD_DCT_RSHIFT(lh - hl, 1);
+      ll += lhmhl_2;
+      hh -= lhmhl_2;
+      lh = ll - lh;
+      hl -= hh;
+      hswap = x & 1;
+      dst[(2*y + vswap)*dstride + 2*x + hswap] = ll;
+      dst[(2*y + vswap)*dstride + 2*x + 1 - hswap] = lh;
+      dst[(2*y + 1 - vswap)*dstride + 2*x + hswap] = hl;
+      dst[(2*y + 1 - vswap)*dstride + 2*x + 1 - hswap] = hh;
     }
   }
 }
 
 /*Increase horizontal and vertical time resolution of a block, splitting it
    into a 2x2 group of blocks.*/
-void od_tf_down_hv(od_coeff *_dst,int _dstride,
- const od_coeff *_src,int _sstride,int _n){
+void od_tf_down_hv(od_coeff *dst, int dstride,
+ const od_coeff *src, int sstride, int n) {
   int x;
   int y;
-  OD_ASSERT(!(_n&1));
-  for(y=0;y<_n>>1;y++){
+  OD_ASSERT(!(n & 1));
+  for (y = 0; y < n >> 1; y++) {
     int vswap;
-    vswap=y&1;
-    for(x=0;x<_n>>1;x++){
+    vswap = y & 1;
+    for (x = 0; x < n >> 1; x++) {
       od_coeff ll;
       od_coeff lh;
       od_coeff hl;
       od_coeff hh;
       od_coeff lhmhl_2;
-      int      hswap;
-      hswap=x&1;
-      ll=_src[(2*y+vswap)*_sstride+2*x+hswap];
-      lh=_src[(2*y+vswap)*_sstride+2*x+1-hswap];
-      hl=_src[(2*y+1-vswap)*_sstride+2*x+hswap];
-      hh=_src[(2*y+1-vswap)*_sstride+2*x+1-hswap];
+      int hswap;
+      hswap = x & 1;
+      ll = src[(2*y + vswap)*sstride + 2*x + hswap];
+      lh = src[(2*y + vswap)*sstride + 2*x + 1 - hswap];
+      hl = src[(2*y + 1 - vswap)*sstride+2*x + hswap];
+      hh = src[(2*y + 1 - vswap)*sstride+2*x + 1 - hswap];
       /*This kernel is identical to that of od_tf_up_hv with the roles of
          hl and lh swapped.*/
-      lh=ll-lh;
-      hl+=hh;
-      lhmhl_2=OD_DCT_RSHIFT(lh-hl,1);
-      ll-=lhmhl_2;
-      hh+=lhmhl_2;
-      hl=ll-hl;
-      lh-=hh;
-      _dst[y*_dstride+x]=ll;
-      _dst[y*_dstride+x+_n]=lh;
-      _dst[(y+_n)*_dstride+x]=hl;
-      _dst[(y+_n)*_dstride+x+_n]=hh;
+      lh = ll - lh;
+      hl += hh;
+      lhmhl_2 = OD_DCT_RSHIFT(lh - hl, 1);
+      ll -= lhmhl_2;
+      hh += lhmhl_2;
+      hl = ll - hl;
+      lh -= hh;
+      dst[y*dstride + x] = ll;
+      dst[y*dstride + x + n] = lh;
+      dst[(y + n)*dstride + x] = hl;
+      dst[(y + n)*dstride + x + n] = hh;
     }
   }
 }
