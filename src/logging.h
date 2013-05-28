58f4fec81b193cd21d36e82b39be89f244db1e48
Timothy B. Terriberry <tterribe@xiph.org>
2013-05-27 19:25:04 -0700

    Enable/disable OD_LOG_PARTIAL properly.

--- src/logging.h
+++ src/logging.h
@@ -93,14 +93,15 @@ int od_log_init(od_logger_function logger);
 
 #ifndef OD_LOGGING_ENABLED
 # define OD_LOG(a)
+/*Hack to accomodate non-newline printfs.*/
+# define OD_LOG_PARTIAL(a)
 # define od_logging_active(a, b) 0
 #else
 # define OD_LOG(a) od_log a
+/*Hack to accomodate non-newline printfs.*/
+# define OD_LOG_PARTIAL(a) od_log_partial a
 # define od_logging_active od_logging_active_impl
 #endif
-
-/* Hack to accomodate non-newline printfs */
-# define OD_LOG_PARTIAL(a) od_log_partial a
 
 int od_log(od_log_facility fac, od_log_level level,
            const char *fmt, ...)
