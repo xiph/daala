487fe06386bd3171e45117f27fd8147fd30c7344
Gregory Maxwell <greg@xiph.org>
2013-06-12 15:25:07 -0700

    Coverage test script improvements.

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
+#!/bin/bash -e
 # continuous integration test script
 # run this from the top-level source directory
+
+# In Jenkins scripts like this should generally be sourced so that they
+#  run in the jenkins controlled shell environment so that errors get noticed.
 
 # run the lcov code-coverage analysis tool
 
 LCOV_PATH=/opt/lcov/bin
 LCOV=${LCOV_PATH}/lcov
 GENHTML=${LCOV_PATH}/genhtml
+VIDEOS=/usr/local/share/videos
 
-cd unix
-rm -Rf objs
+cd unix && rm -Rf objs
 make clean
 CFLAGS="-g3 -fprofile-arcs -ftest-coverage -UOD_ENABLE_ASSERTIONS" make
+${LCOV} -z -d `pwd` -b `pwd`
+${LCOV} -c -b `pwd` -d `pwd` -t baseline -o baseline.info
 CFLAGS="-g3 -fprofile-arcs -ftest-coverage -UOD_ENABLE_ASSERTIONS" make check
-${LCOV} -c -b `pwd` -d `pwd` -o makecheck.info
-${GENHTML} -s -o coverage/ makecheck.info
+${LCOV} -c -b `pwd` -d `pwd` -t check -o makecheck.info
+${LCOV} -z -d `pwd` -b `pwd`
+rm -f out.ogv
+./encoder_example -k 4 ${VIDEOS}/claire_qcif-2frames.y4m -o out.ogv
+./dump_video out.ogv -o /dev/null
+${LCOV} -c -b `pwd` -d `pwd` -t encoderdecoder -o encdec.info
+${LCOV} -a baseline.info -a makecheck.info -a encdec.info -o daala_coverage.info
+${GENHTML} -s -o coverage/ daala_coverage.info
