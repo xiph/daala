19b43e74ca3160a1e149158d203bf23d9aa92107
Gregory Maxwell <greg@xiph.org>
2013-10-14 16:21:22 -0700

    [ci] Enable OD_CHECKASM and assertions and running the encoder.

--- ci/autotools.sh
+++ ci/autotools.sh
@@ -3,8 +3,12 @@
 # run this from the top-level source directory
 
 OGG_PATH=/srv/jenkins/jobs/libogg/workspace
+VIDEOS=/usr/local/share/videos
 
 ./autogen.sh
-./configure PKG_CONFIG_PATH=${OGG_PATH}
+CFLAGS='-O2 -DOD_CHECKASM -g' ./configure --enable-assertions --enable-logging PKG_CONFIG_PATH=${OGG_PATH}
 make distcheck PKG_CONFIG_PATH=${OGG_PATH}
 make docs
+./examples/encoder_example -k 4 ${VIDEOS}/claire_qcif-2frames.y4m -o out.$$.ogv
+./examples/dump_video out.$$.ogv -o /dev/null
+rm -f out.$$.ogv
