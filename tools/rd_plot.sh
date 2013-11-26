0dda5a9f3452272ca3cbd19d750c8a4300c0ad62
Nathan E. Egge <negge@dgql.org>
2013-11-26 18:01:58 -0500

    Update rd_plot.sh to produce an image for each metric.

--- tools/rd_plot.sh
+++ tools/rd_plot.sh
@@ -3,23 +3,21 @@
 # Use this to average data from multiple runs
 #awk '{size[FNR]+=$2;bytes[FNR]+=$3;psnr[FNR]+=$2*$4;psnrhvs[FNR]+=$2*$5;ssim[FNR]+=$2*$6;fastssim[FNR]+=$2*$7;}END{for(i=1;i<=FNR;i++)print i+1,size[i],bytes[i],psnr[i]/size[i],psnrhvs[i]/size[i],ssim[i]/size[i],fastssim[i]/size[i];}' *.out > total.out
 
-if [ -z "$IMAGE" ]; then
-  IMAGE=psnrhvs.png
+if [ -n "$IMAGE" ]; then
+  IMAGE="$IMAGE-"
 fi
 
 if [ $# == 0 ]; then
-  echo "no parameters"
+  echo "usage: IMAGE=<prefix> $0 *.out"
   exit 1
 fi
 
 CMDS="$CMDS set term png size 1024,768;"
-CMDS="$CMDS set output \"$IMAGE\";"
 CMDS="$CMDS set log x;"
 CMDS="$CMDS set xlabel 'Bits/Pixel';"
 CMDS="$CMDS set ylabel 'dB';"
 CMDS="$CMDS set key bot right;"
 
-CMDS="$CMDS plot"
 for FILE in "$@"; do
   BASENAME=$(basename $FILE)
   PSNR="$PSNR $PREFIX '$FILE' using (\$3*8/\$2):4 with lines title '${BASENAME%.*} (PSNR)'"
@@ -28,16 +26,12 @@ for FILE in "$@"; do
   FASTSSIM="$FASTSSIM $PREFIX '$FILE' using (\$3*8/\$2):7 with lines title '${BASENAME%.*} (FAST SSIM)'"
   PREFIX=","
 done
-PREFIX=""
-CMDS="$CMDS $PREFIX $PSNR"
-PREFIX=","
-CMDS="$CMDS $PREFIX $PSNRHVS"
-PREFIX=","
-#CMDS="$CMDS $PREFIX $SSIM"
-#PREFIX=","
-#CMDS="$CMDS $PREFIX $FASTSSIM"
-#PREFIX=","
-echo $CMDS
-CMDS="$CMDS;"
 
-gnuplot -e "$CMDS" 2> /dev/null
+SUFFIX="psnr.png"
+gnuplot -e "$CMDS set output \"$IMAGE$SUFFIX\"; plot $PSNR;"     2> /dev/null
+SUFFIX="psnrhvs.png"
+gnuplot -e "$CMDS set output \"$IMAGE$SUFFIX\"; plot $PSNRHVS;"  2> /dev/null
+SUFFIX="ssim.png"
+gnuplot -e "$CMDS set output \"$IMAGE$SUFFIX\"; plot $SSIM;"     2> /dev/null
+SUFFIX="fastssim.png"
+gnuplot -e "$CMDS set output \"$IMAGE$SUFFIX\"; plot $FASTSSIM;" 2> /dev/null
