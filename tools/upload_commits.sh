d9241808e21b098d4487f23b13d226c00003bd34
Jack Moffitt <jack@metajack.im>
2014-02-04 19:21:58 -0700

    Add tools/upload_commits.sh.
    This script makes multi-commit reviews fun!

---
+++ tools/upload_commits.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# USAGE: upload_commits.sh <num_commits> [upload.py ARGS]
+# NOTES: -y and --send_mail are automatically passed to upload.py
+
+if [ $# == 0 ]; then
+  echo "usage: $0 <num_commits> [upload.py ARGS]"
+  exit 1
+fi
+
+dir=$(dirname $0)
+
+COMMITS=$1
+shift
+
+for i in $(seq $COMMITS -1 1)
+do
+    msg=$(git log --pretty=format:%s -n 1 HEAD~$((i - 1)))
+    ${dir}/upload.py -y --send_mail $@ --rev HEAD~$i..HEAD~$((i - 1)) -m "$msg"
+done
