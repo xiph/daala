2fb108cb397d29c49c091a487b0202830356c28a
Nathan E. Egge <negge@dgql.org>
2014-06-09 17:13:00 -0400

    Fixing rd_collect using new reconstructed filename.

--- tools/rd_collect_daala.sh
+++ tools/rd_collect_daala.sh
@@ -12,17 +12,17 @@ BASENAME=$(basename $FILE)
 rm $BASENAME.out 2> /dev/null || true
 echo $BASENAME
 
-RANGE=$(seq 2 40)
+RANGE="1 2 3 4 5 6 7 9 11 13 16 20 25 30 37 45 55 67 81 99 122 148 181 221 270 330 400 500"
 
 for x in $RANGE; do
   OD_LOG_MODULES='encoder:10' OD_DUMP_IMAGES_SUFFIX=$BASENAME $ENCODER_EXAMPLE -v $x $FILE -o /dev/null 2> $BASENAME-$x-enc.out
   PIXELS=$(cat $BASENAME-$x-enc.out | grep "Plane $PLANE" | sed -re 's/^.*Pixels:\ +([0-9]+).*$/\1/')
   PSNR=$(cat $BASENAME-$x-enc.out | grep "Plane $PLANE" | sed -re 's/^.*PSNR:\ +([0-9\.]+).*$/\1/')
   SIZE=$(cat $BASENAME-$x-enc.out | grep 'Output' | sed -re 's/^.*Bytes:\ +([0-9]+).*$/\1/')
-  PSNRHVS=$($DUMP_PSNRHVS $FILE 00000000$BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  SSIM=$($DUMP_SSIM $FILE 00000000$BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  FASTSSIM=$($DUMP_FASTSSIM -c $FILE 00000000$BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  rm 00000000$BASENAME.y4m $BASENAME-$x-enc.out
+  PSNRHVS=$($DUMP_PSNRHVS $FILE 00000000out-$BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+  SSIM=$($DUMP_SSIM $FILE 00000000out-$BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+  FASTSSIM=$($DUMP_FASTSSIM -c $FILE 00000000out-$BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+  rm 00000000out-$BASENAME.y4m $BASENAME-$x-enc.out
   echo $x $PIXELS $SIZE $PSNR $PSNRHVS $SSIM $FASTSSIM >> $BASENAME.out
   #tail -1 $BASENAME.out
 done
