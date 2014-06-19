087adcfafd7096fcb8102385f7b47b93745f3d62
Nathan E. Egge <negge@dgql.org>
2014-06-19 12:20:33 -0400

    Fixing typo in arugment 2 of the usage.

--- tools/yuvjpeg.c
+++ tools/yuvjpeg.c
@@ -119,7 +119,7 @@ int main(int argc, char *argv[]) {
   if (argc != 5) {
     fprintf(stderr, "Required arguments:\n");
     fprintf(stderr, "1. JPEG quality value, 0-100\n");
-    fprintf(stderr, "2. Image size (e.g. '512x512'\n");
+    fprintf(stderr, "2. Image size (e.g. '512x512')\n");
     fprintf(stderr, "3. Path to YUV input file\n");
     fprintf(stderr, "4. Path to JPG output file\n");
     return 1;
