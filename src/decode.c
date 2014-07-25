xiph/daala

commit d6ebba54bb9b137af45aa180301d023390278e99

Joint skip symbol for both AC and DC
Conflicts:
	src/encode.c
	src/pvq_encoder.c

src/decode.c
@@ -359,15 +359,6 @@ void od_single_band_decode(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, int ln,
   else
     dc_scale = (pli==0 || dec->scale[pli]==0) ? scale : (scale + 1) >> 1;
   coeff_shift = dec->scale[pli] == 0 ? 0 : OD_COEFF_SHIFT;
-  if (OD_DISABLE_HAAR_DC || !ctx->is_keyframe) {
-    pred[0] = generic_decode(&dec->ec, &dec->adapt.model_dc[pli], -1,
-     &dec->adapt.ex_dc[pli][ln][0], 2);
-    if (pred[0]) pred[0] *= od_ec_dec_bits(&dec->ec, 1) ? -1 : 1;
-    pred[0] = (pred[0]*dc_scale << coeff_shift) + predt[0];
-  }
-  else {
-    pred[0] = d[((by << 2))*w + ((bx << 2))];
-  }
   if (run_pvq) {
     pvq_decode(dec, predt, pred, scale << coeff_shift, ln,
      OD_PVQ_QM_Q4[pli][ln], OD_PVQ_BETA[pli][ln],
@@ -376,6 +367,17 @@ void od_single_band_decode(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, int ln,
   else {
     od_single_band_scalar_decode(dec, ln, pred, predt, scale, pli,
      coeff_shift);
+  }
+  if (OD_DISABLE_HAAR_DC || !ctx->is_keyframe) {
+    if (!run_pvq || pred[0]) {
+      pred[0] = run_pvq + generic_decode(&dec->ec, &dec->adapt.model_dc[pli],
+       -1, &dec->adapt.ex_dc[pli][ln][0], 2);
+      if (pred[0]) pred[0] *= od_ec_dec_bits(&dec->ec, 1) ? -1 : 1;
+    }
+    pred[0] = (pred[0]*dc_scale << coeff_shift) + predt[0];
+  }
+  else {
+    pred[0] = d[((by << 2))*w + ((bx << 2))];
   }
 #ifdef USE_BAND_PARTITIONS
   od_coding_order_to_raster(&d[((by << 2))*w + (bx << 2)], w, pred, n, !run_pvq);

src/encode.c
@@ -538,7 +538,19 @@ void od_block_encode(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, int ln,
     scalar_out[0] = OD_DIV_R0(cblock[0] - predt[0], dc_scale << coeff_shift);
     OD_ACCT_UPDATE(&enc->acct, od_ec_enc_tell_frac(&enc->ec),
      OD_ACCT_CAT_TECHNIQUE, OD_ACCT_TECH_DC_COEFF);
-    generic_encode(&enc->ec, &enc->adapt.model_dc[pli], abs(scalar_out[0]), -1,
+  }
+  if (run_pvq) {
+    pvq_encode(enc, predt, cblock, scalar_out, scale << coeff_shift, ln,
+     OD_PVQ_QM_Q4[pli][ln], OD_PVQ_BETA[pli][ln],
+     OD_PVQ_INTER_BAND_MASKING[ln], ctx->is_keyframe);
+  }
+  else {
+    od_single_band_scalar_quant(enc, ln, scalar_out, cblock, predt, scale, pli,
+     coeff_shift);
+  }
+  if (OD_DISABLE_HAAR_DC || !ctx->is_keyframe) {
+    if (!run_pvq || scalar_out[0]) generic_encode(&enc->ec,
+     &enc->adapt.model_dc[pli], abs(scalar_out[0]) - run_pvq, -1,
      &enc->adapt.ex_dc[pli][ln][0], 2);
     if (scalar_out[0]) od_ec_enc_bits(&enc->ec, scalar_out[0] < 0, 1);
     OD_ACCT_UPDATE(&enc->acct, od_ec_enc_tell_frac(&enc->ec),
@@ -551,15 +563,6 @@ void od_block_encode(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, int ln,
   }
   OD_ACCT_UPDATE(&enc->acct, od_ec_enc_tell_frac(&enc->ec),
    OD_ACCT_CAT_TECHNIQUE, OD_ACCT_TECH_AC_COEFFS);
-  if (run_pvq) {
-    pvq_encode(enc, predt, cblock, scalar_out, scale << coeff_shift, ln,
-     OD_PVQ_QM_Q4[pli][ln], OD_PVQ_BETA[pli][ln],
-     OD_PVQ_INTER_BAND_MASKING[ln], ctx->is_keyframe);
-  }
-  else {
-    od_single_band_scalar_quant(enc, ln, scalar_out, cblock, predt, scale, pli,
-     coeff_shift);
-  }
   OD_ACCT_UPDATE(&enc->acct, od_ec_enc_tell_frac(&enc->ec),
    OD_ACCT_CAT_TECHNIQUE, OD_ACCT_TECH_UNKNOWN);
 #ifdef USE_BAND_PARTITIONS

src/pvq_decoder.c
@@ -194,6 +194,7 @@ void pvq_decode(daala_dec_ctx *dec,
   double g[PVQ_MAX_PARTITIONS] = {0};
   generic_encoder *model;
   unsigned *noref_prob;
+  int skip;
   adapt = dec->adapt.pvq_adapt;
   exg = dec->adapt.pvq_exg + ln*PVQ_MAX_PARTITIONS;
   ext = dec->adapt.pvq_ext + ln*PVQ_MAX_PARTITIONS;
@@ -201,14 +202,17 @@ void pvq_decode(daala_dec_ctx *dec,
   model = dec->adapt.pvq_param_model;
   nb_bands = od_band_offsets[ln][0];
   off = &od_band_offsets[ln][1];
-  if (!is_keyframe && od_ec_decode_bool_q15(&dec->ec, dec->adapt.skip_prob)) {
+  if (is_keyframe) skip = 0;
+  else {
+    skip = od_decode_cdf_adapt(&dec->ec, dec->adapt.skip_cdf, 4,
+     dec->adapt.skip_increment);
+    out[0] = skip&1;
+    skip >>= 1;
+  }
+  if (skip) {
     for (i = 1; i < 1 << (2*ln + 4); i++) out[i] = ref[i];
-    dec->adapt.skip_prob = OD_CLAMPI(2000, dec->adapt.skip_prob
-     - (dec->adapt.skip_prob >> 4), 30000);
   }
   else {
-    dec->adapt.skip_prob = OD_CLAMPI(2000, dec->adapt.skip_prob
-     - ((dec->adapt.skip_prob - 32768) >> 4), 30000);
     for (i = 0; i < nb_bands; i++) size[i] = off[i+1] - off[i];
     for (i = 0; i < nb_bands; i++) {
       if (is_keyframe && vector_is_null(ref + off[i], size[i])) noref[i] = 1;

src/pvq_encoder.c
@@ -145,6 +145,7 @@ void pvq_encode(daala_enc_ctx *enc,
   generic_encoder *model;
   unsigned *noref_prob;
   double skip_acc;
+  int skip;
   adapt = enc->adapt.pvq_adapt;
   exg = enc->adapt.pvq_exg + ln*PVQ_MAX_PARTITIONS;
   ext = enc->adapt.pvq_ext + ln*PVQ_MAX_PARTITIONS;
@@ -164,15 +165,14 @@ void pvq_encode(daala_enc_ctx *enc,
      q*qm[i + 1] >> 4, y + off[i], &theta[i], &max_theta[i], &k[i], &g[i],
      beta[i], &skip_acc);
   }
-  if (!is_keyframe && skip_acc < 0) {
-    od_ec_encode_bool_q15(&enc->ec, 1, enc->adapt.skip_prob);
+  skip = !is_keyframe && skip_acc < 0;
+  if (!is_keyframe) {
+    od_encode_cdf_adapt(&enc->ec, 2*skip + (out[0] != 0), enc->adapt.skip_cdf,
+     4, enc->adapt.skip_increment);
+  }
+  if (skip) {
     for (i = 1; i < 1 << (2*ln + 4); i++) out[i] = ref[i];
-    enc->adapt.skip_prob = OD_CLAMPI(2000, enc->adapt.skip_prob
-     - (enc->adapt.skip_prob >> 4), 30000);
   } else {
-    if (!is_keyframe) od_ec_encode_bool_q15(&enc->ec, 0, enc->adapt.skip_prob);
-    enc->adapt.skip_prob = OD_CLAMPI(2000, enc->adapt.skip_prob
-     - ((enc->adapt.skip_prob - 32768) >> 4), 30000);
     /* TODO: Find efficient way to code up to 4 noref flags per symbol
        to reduce entropy coder calls. */
     for (i = 0; i < nb_bands; i++) {

src/state.c
@@ -246,7 +246,8 @@ void od_adapt_ctx_reset(od_adapt_ctx *state, int is_keyframe) {
   }
   generic_model_init(&state->mv_model);
   for (i = 0; i < 5; i++) state->mv_ex[i] = state->mv_ey[i] = 24 << 16;
-  state->skip_prob = 8192;
+  state->skip_increment = 128;
+  for (i = 0; i < 4; i++) state->skip_cdf[i] = (i + 1)*state->skip_increment;
   state->mv_small_increment = 128;
   state->mv_small_cdf[0] = 10*state->mv_small_increment;
   for (i = 1; i < 16; i++) {

src/state.h
@@ -183,7 +183,9 @@ struct od_adapt_ctx {
   int ex_dc[OD_NPLANES_MAX][OD_NBSIZES][3];
   int ex_g[OD_NPLANES_MAX][OD_NBSIZES];
 
-  unsigned skip_prob;
+  /* Joint skip flag for DC and AC */
+  ogg_uint16_t skip_cdf[4];
+  int skip_increment;
 };
 
 int od_state_init(od_state *_state, const daala_info *_info);

commit f4db3996cf3f64887273c2c3c273308bbedd7be7

Fixes intra pred bug introduced in d6ebba54b
DC was assumed zero even when Haar was off.

src/decode.c
@@ -369,9 +369,11 @@ void od_single_band_decode(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, int ln,
      coeff_shift);
   }
   if (OD_DISABLE_HAAR_DC || !ctx->is_keyframe) {
-    if (!run_pvq || pred[0]) {
-      pred[0] = run_pvq + generic_decode(&dec->ec, &dec->adapt.model_dc[pli],
-       -1, &dec->adapt.ex_dc[pli][ln][0], 2);
+    int has_dc_skip;
+    has_dc_skip = !ctx->is_keyframe && run_pvq;
+    if (!has_dc_skip || pred[0]) {
+      pred[0] = has_dc_skip + generic_decode(&dec->ec,
+       &dec->adapt.model_dc[pli], -1, &dec->adapt.ex_dc[pli][ln][0], 2);
       if (pred[0]) pred[0] *= od_ec_dec_bits(&dec->ec, 1) ? -1 : 1;
     }
     pred[0] = (pred[0]*dc_scale << coeff_shift) + predt[0];

src/encode.c
@@ -554,9 +554,12 @@ void od_block_encode(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, int ln,
   OD_ACCT_UPDATE(&enc->acct, od_ec_enc_tell_frac(&enc->ec),
     OD_ACCT_CAT_TECHNIQUE, OD_ACCT_TECH_UNKNOWN);
   if (OD_DISABLE_HAAR_DC || !ctx->is_keyframe) {
-    if (!run_pvq || scalar_out[0]) generic_encode(&enc->ec,
-     &enc->adapt.model_dc[pli], abs(scalar_out[0]) - run_pvq, -1,
-     &enc->adapt.ex_dc[pli][ln][0], 2);
+    int has_dc_skip;
+    has_dc_skip = !ctx->is_keyframe && run_pvq;
+    if (!has_dc_skip || scalar_out[0]) {
+      generic_encode(&enc->ec, &enc->adapt.model_dc[pli],
+       abs(scalar_out[0]) - has_dc_skip, -1, &enc->adapt.ex_dc[pli][ln][0], 2);
+    }
     if (scalar_out[0]) od_ec_enc_bits(&enc->ec, scalar_out[0] < 0, 1);
     OD_ACCT_UPDATE(&enc->acct, od_ec_enc_tell_frac(&enc->ec),
      OD_ACCT_CAT_TECHNIQUE, OD_ACCT_TECH_UNKNOWN);