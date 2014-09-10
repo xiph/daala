44deb349fec55b4f0be02cbbe9d3f51860a6ab0b
Thomas Daede <daede003@umn.edu>
2014-09-10 16:15:33 -0700

    Enable the SSE2 4x4 IDCT
    This should have been enabled earlier but slipped through.

--- src/x86/x86state.c
+++ src/x86/x86state.c
@@ -43,6 +43,7 @@ void od_state_opt_vtbl_init_x86(od_state *_state){
     _state->opt_vtbl.mc_blend_full_split8=od_mc_blend_full_split8_sse2;
 #if defined(OD_SSE2_INTRINSICS)
     _state->opt_vtbl.fdct_2d[0]=od_bin_fdct4x4_sse2;
+    _state->opt_vtbl.idct_2d[0]=od_bin_idct4x4_sse2;
 #endif
 #if defined(OD_SSE41_INTRINSICS)
     if (_state->cpu_flags&OD_CPU_X86_SSE4_1){
