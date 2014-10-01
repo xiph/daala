ee665332e631aecf6d74aaedbfde2491762e3690
Jean-Marc Valin <jmvalin@jmvalin.ca>
2014-10-01 17:44:57 -0400

    per-superblock strength

--- src/intra_paint_enc.c
+++ src/intra_paint_enc.c
@@ -683,7 +683,7 @@ void od_intra_paint_compute_edges(od_adapt_ctx *adapt, od_ec_enc *enc, unsigned 
 #if 1
 #define VAR2(q) do {int yy;\
                 yy = COVAR(edge_sum2[idx], edge_sum1[idx], edge_sum1[idx], edge_count[idx]); \
-                paint[idx] = OD_CLAMPI(0, (int)(256.*((q)*(q)/12./4)/(10+yy)), 255);} while(0)
+                paint[idx] = OD_CLAMPI(0, (int)(256.*((q)*(q)/12./64)/(10+yy)), 255);} while(0)
 #else
 #define VAR2(q) do {int yy, xy;\
                 yy = COVAR(edge_sum2[idx], edge_sum1[idx], edge_sum1[idx], edge_count[idx]); \
@@ -1562,6 +1562,8 @@ int *edge_corr = var_count+4096;
 unsigned char *paint_mask=mask+4096;
 unsigned char *paint_out=paint_buf+4096;
 
+ogg_uint16_t gain_cdf[] = {128, 256, 384, 512, 640, 768, 896, 1024, 1152};
+
 void od_paint_dering(od_adapt_ctx *adapt, od_ec_enc *enc, unsigned char *paint, const unsigned char *img,
  int w, int h, int stride, const unsigned char *dec8, int bstride,
  unsigned char *mode, int mstride, int *edge_sum, int *edge_count, int q,
@@ -1612,6 +1614,56 @@ void od_paint_dering(od_adapt_ctx *adapt, od_ec_enc *enc, unsigned char *paint, 
        mstride, edge_sum, edge_count, q, res, j, i, 3);
     }
   }
+#if 1
+  for(i = 0; i < h; i++) {
+    for(j = 0; j < w; j++) {
+      int idx;
+      int gi;
+      int k;
+      int m;
+      int dist;
+      int best_dist;
+      int best_gain;
+      best_gain = 0;
+      dist = 0;
+      for (k = 0; k < 32; k++) {
+        for (m = 0; m < 32; m++) {
+          idx = (32*i+k)*stride + 32*j + m;
+          dist += (img[idx] - paint[idx])*(int)(img[idx] - paint[idx]);
+        }
+      }
+      best_dist = dist;
+      for (gi = 1; gi <= 4; gi++) {
+        dist = 0;
+        for (k = 0; k < 32; k++) {
+          for (m = 0; m < 32; m++) {
+            int x;
+            int y;
+            idx = (32*i+k)*stride + 32*j + m;
+            x = img[idx];
+            y = OD_CLAMPI(0, paint[idx] + ((OD_MINI((int)paint_mask[idx]<<gi>>1, 255)*(paint_out[idx] - paint[idx]) + 128) >> 8), 255);
+            dist += (x - y)*(int)(x - y);
+          }
+        }
+        if (dist < best_dist) {
+          best_dist = dist;
+          best_gain = gi;
+          for (k = 0; k < 32; k++) {
+            for (m = 0; m < 32; m++) {
+              int y;
+              idx = (32*i+k)*stride + 32*j + m;
+              y = OD_CLAMPI(0, paint[idx] + ((OD_MINI((int)paint_mask[idx]<<gi>>1, 255)*(paint_out[idx] - paint[idx]) + 128) >> 8), 255);
+              paint[idx] = y;
+            }
+          }
+        }
+      }
+      od_encode_cdf_adapt(enc, best_gain, gain_cdf, 9, 128);
+      /*printf("%d ", best_gain);*/
+    }
+    /*printf("\n");*/
+  }
+#else
   for(i = 0; i < 32*h; i++) {
     for(j = 0; j < 32*w; j++) {
       int idx;
@@ -1630,6 +1682,7 @@ void od_paint_dering(od_adapt_ctx *adapt, od_ec_enc *enc, unsigned char *paint, 
         mstride, edge_sum, edge_count, q, res, j, i, 3);
     }
   }
+#endif
 #endif
 }
 
