xiph/daala

commit f1a9307c8752883e8692476de72a0e1f9665ba30

Add a 32x32 transform (and inverse).
The transform uses TFed 16x16s. This also enables 32x32 blocks in the codec.

src/pvq.c
@@ -34,7 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 #include <string.h>
 #include "filter.h"
 
-#define MAXN 256
+#define MAXN 512
 #define EPSILON 1e-30
 
 /* This is the PVQ equivalent of a quantization matrix, except that
@@ -46,15 +46,21 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 static const int OD_PVQ_QM4_LUMA_Q4[2] = {16, 16};
 static const int OD_PVQ_QM8_LUMA_Q4[5] = {16, 16, 16, 16, 16};
 static const int OD_PVQ_QM16_LUMA_Q4[8] = {16, 16, 16, 16, 16, 16, 16, 16};
+static const int OD_PVQ_QM32_LUMA_Q4[11] = {16, 16, 16, 16, 16, 16, 16, 16,
+ 16, 16, 16};
 # else
 static const int OD_PVQ_QM4_LUMA_Q4[2] = {8, 16};
 static const int OD_PVQ_QM8_LUMA_Q4[5] = {16, 16, 32, 32, 64};
 static const int OD_PVQ_QM16_LUMA_Q4[8] = {16, 16, 16, 16, 32, 32, 32, 64};
+static const int OD_PVQ_QM32_LUMA_Q4[11] = {16, 16, 16, 16, 16, 16, 16, 32,
+ 32, 32, 64};
 # endif
 
 static const double OD_PVQ_BETA4_LUMA[1] = {1.};
 static const double OD_PVQ_BETA8_LUMA[4] = {1., 1., 1., 1.};
 static const double OD_PVQ_BETA16_LUMA[7] = {1., 1., 1., 1., 1., 1., 1.};
+static const double OD_PVQ_BETA32_LUMA[10] = {1., 1., 1., 1., 1., 1., 1.,
+ 1., 1., 1.};
 
 #else
 
@@ -65,10 +71,14 @@ static const double OD_PVQ_BETA16_LUMA[7] = {1., 1., 1., 1., 1., 1., 1.};
 static const int OD_PVQ_QM4_LUMA_Q4[2] = {19, 19};
 static const int OD_PVQ_QM8_LUMA_Q4[5] = {16, 16, 44, 44, 72};
 static const int OD_PVQ_QM16_LUMA_Q4[8] = {16, 13, 18, 18, 36, 40, 40, 80};
+static const int OD_PVQ_QM32_LUMA_Q4[11] = {16, 12, 14, 14, 12, 14, 14, 32,
+ 36, 36, 64};
 
 static const double OD_PVQ_BETA4_LUMA[1] = {1.};
 static const double OD_PVQ_BETA8_LUMA[4] = {1.5, 1.5, 1.5, 1.5};
 static const double OD_PVQ_BETA16_LUMA[7] = {1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5};
+static const double OD_PVQ_BETA32_LUMA[10] = {1.5, 1.5, 1.5, 1.5, 1.5, 1.5,
+1.5, 1.5, 1.5, 1.5};
 
 #endif
 
@@ -77,38 +87,55 @@ static const double OD_PVQ_BETA16_LUMA[7] = {1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5};
 static const int OD_PVQ_QM4_CB_Q4[2] = {8, 16};
 static const int OD_PVQ_QM8_CB_Q4[5] = {8, 16, 16, 16, 16};
 static const int OD_PVQ_QM16_CB_Q4[8] = {8, 16, 16, 16, 16, 16, 16, 16};
+static const int OD_PVQ_QM32_CB_q4[11] = {8, 16, 16, 16, 16, 16, 16, 16,
+ 16, 16, 16};
 
 #define OD_PVQ_QM4_CR_Q4 OD_PVQ_QM4_CB_Q4
 #define OD_PVQ_QM8_CR_Q4 OD_PVQ_QM8_CB_Q4
 #define OD_PVQ_QM16_CR_Q4 OD_PVQ_QM16_CB_Q4
+#define OD_PVQ_QM32_CR_Q4 OD_PVQ_QM32_CB_Q4
 #else
 static const int OD_PVQ_QM4_CB_Q4[2] = {14, 32};
 static const int OD_PVQ_QM8_CB_Q4[5] = {10, 48, 64, 64, 100};
 static const int OD_PVQ_QM16_CB_Q4[8] = {16, 32, 48, 48, 64, 64, 64, 100};
+static const int OD_PVQ_QM32_CB_Q4[11] = {16, 32, 32, 32, 32, 48, 48, 64,
+ 64, 64, 100};
 
 static const int OD_PVQ_QM4_CR_Q4[2] = {12, 16};
 static const int OD_PVQ_QM8_CR_Q4[5] = {8, 24, 64, 64, 100};
 static const int OD_PVQ_QM16_CR_Q4[8] = {8, 16, 24, 24, 32, 64, 64, 100};
+static const int OD_PVQ_QM32_CR_Q4[11] = {8, 16, 16, 16, 16, 24, 24, 32,
+ 64, 64, 100};
 #endif
 
 static const double OD_PVQ_BETA4_CHROMA[1] = {1.};
 static const double OD_PVQ_BETA8_CHROMA[4] = {1., 1., 1., 1.};
 static const double OD_PVQ_BETA16_CHROMA[7] = {1., 1., 1., 1., 1., 1., 1.};
+static const double OD_PVQ_BETA32_CHROMA[10] = {1., 1., 1., 1., 1., 1., 1.,
+ 1., 1., 1.};
 
 /* We use the chroma params for the alpha channel. Not sure whether it's
    a good idea. */
 const int *const OD_PVQ_QM_Q4[OD_NPLANES_MAX][OD_NBSIZES] = {
-  {OD_PVQ_QM4_LUMA_Q4, OD_PVQ_QM8_LUMA_Q4, OD_PVQ_QM16_LUMA_Q4},
-  {OD_PVQ_QM4_CB_Q4, OD_PVQ_QM8_CB_Q4, OD_PVQ_QM16_CB_Q4},
-  {OD_PVQ_QM4_CR_Q4, OD_PVQ_QM8_CR_Q4, OD_PVQ_QM16_CR_Q4},
-  {OD_PVQ_QM4_CR_Q4, OD_PVQ_QM8_CR_Q4, OD_PVQ_QM16_CR_Q4}
+  {OD_PVQ_QM4_LUMA_Q4, OD_PVQ_QM8_LUMA_Q4,
+   OD_PVQ_QM16_LUMA_Q4, OD_PVQ_QM32_LUMA_Q4},
+  {OD_PVQ_QM4_CB_Q4, OD_PVQ_QM8_CB_Q4,
+   OD_PVQ_QM16_CB_Q4, OD_PVQ_QM32_CB_Q4},
+  {OD_PVQ_QM4_CR_Q4, OD_PVQ_QM8_CR_Q4,
+   OD_PVQ_QM16_CR_Q4, OD_PVQ_QM32_CR_Q4},
+  {OD_PVQ_QM4_CR_Q4, OD_PVQ_QM8_CR_Q4,
+   OD_PVQ_QM16_CR_Q4, OD_PVQ_QM32_CR_Q4}
 };
 
 const double *const OD_PVQ_BETA[OD_NPLANES_MAX][OD_NBSIZES] = {
-  {OD_PVQ_BETA4_LUMA, OD_PVQ_BETA8_LUMA, OD_PVQ_BETA16_LUMA},
-  {OD_PVQ_BETA4_CHROMA, OD_PVQ_BETA8_CHROMA, OD_PVQ_BETA16_CHROMA},
-  {OD_PVQ_BETA4_CHROMA, OD_PVQ_BETA8_CHROMA, OD_PVQ_BETA16_CHROMA},
-  {OD_PVQ_BETA4_CHROMA, OD_PVQ_BETA8_CHROMA, OD_PVQ_BETA16_CHROMA}
+  {OD_PVQ_BETA4_LUMA, OD_PVQ_BETA8_LUMA,
+   OD_PVQ_BETA16_LUMA, OD_PVQ_BETA32_LUMA},
+  {OD_PVQ_BETA4_CHROMA, OD_PVQ_BETA8_CHROMA,
+   OD_PVQ_BETA16_CHROMA, OD_PVQ_BETA32_CHROMA},
+  {OD_PVQ_BETA4_CHROMA, OD_PVQ_BETA8_CHROMA,
+   OD_PVQ_BETA16_CHROMA, OD_PVQ_BETA32_CHROMA},
+  {OD_PVQ_BETA4_CHROMA, OD_PVQ_BETA8_CHROMA,
+   OD_PVQ_BETA16_CHROMA, OD_PVQ_BETA32_CHROMA}
 };
 
 /** Find the codepoint on the given PSphere closest to the desired

.gitignore
@@ -64,6 +64,7 @@ examples/encoder_example
 examples/player_example
 src/tests/check_tests
 src/tests/dcttest
+src/tests/test_32x32
 src/tests/ectest
 src/tests/logging_test
 src/tests/test_coef_coder

Makefile.am
@@ -41,7 +41,7 @@ noinst_HEADERS = \
 	src/x86/x86enc.h \
 	src/x86/x86int.h
 
-src_dct_SOURCES = src/dct.c
+src_dct_SOURCES = src/dct.c src/tf.c src/internal.c
 if ENABLE_X86ASM
 src_dct_SOURCES += src/x86/cpu.c
 if ENABLE_SSE2_INTRINSICS
@@ -69,7 +69,6 @@ src_libdaalabase_la_SOURCES = \
 	src/generic_code.c \
 	src/generic_decoder.c \
 	src/info.c \
-	src/internal.c \
 	src/intra.c \
 	src/intradata.c \
 	src/laplace_decoder.c \
@@ -82,10 +81,10 @@ src_libdaalabase_la_SOURCES = \
 	src/pvq_decoder.c \
 	src/state.c \
 	src/switch_table.c \
-	src/tf.c \
 	src/zigzag4.c \
 	src/zigzag8.c \
 	src/zigzag16.c \
+	src/zigzag32.c \
 	$(src_dct_SOURCES)
 if ENABLE_X86ASM
 src_libdaalabase_la_SOURCES += \
@@ -269,7 +268,6 @@ tools_block_size_analysis_SOURCES = \
 	tools/vidinput.c \
 	tools/y4m_input.c \
 	$(src_dct_SOURCES) \
-	src/internal.c \
 	src/generic_encoder.c \
 	src/generic_code.c \
 	src/laplace_encoder.c \
@@ -333,8 +331,6 @@ tools_intra_stats_SOURCES = \
 	$(src_dct_SOURCES) \
 	src/intra.c \
 	src/switch_table.c \
-	src/tf.c \
-	src/internal.c \
 	src/intradata.c
 tools_intra_stats_CFLAGS = $(THEORA_CFLAGS) $(OGG_CFLAGS) $(PNG_CFLAGS) $(OPENMP_CFLAGS)
 tools_intra_stats_LDADD = $(THEORA_LIBS) $(OGG_LIBS) $(PNG_LIBS) $(LIBM)
@@ -367,9 +363,7 @@ tools_intra_pred_SOURCES = \
 	$(src_dct_SOURCES) \
 	src/intra.c \
 	src/intradata.c \
-	src/internal.c \
-	src/switch_table.c \
-	src/tf.c
+	src/switch_table.c
 tools_intra_pred_CFLAGS = $(OGG_CFLAGS) $(PNG_CFLAGS) $(OPENMP_CFLAGS)
 tools_intra_pred_LDADD = $(THEORA_LIBS) $(OGG_LIBS) $(PNG_LIBS) $(LIBM)
 
@@ -400,8 +394,6 @@ tools_intra_trace_SOURCES = \
 	$(src_dct_SOURCES) \
 	src/intra.c \
 	src/switch_table.c \
-	src/tf.c \
-	src/internal.c \
 	src/intradata.c
 tools_intra_trace_CFLAGS = $(OGG_CFLAGS) $(PNG_CFLAGS) $(OPENMP_CFLAGS)
 tools_intra_trace_LDADD = $(THEORA_LIBS) $(OGG_LIBS) $(PNG_LIBS) $(LIBM)
@@ -409,7 +401,6 @@ tools_intra_trace_LDADD = $(THEORA_LIBS) $(OGG_LIBS) $(PNG_LIBS) $(LIBM)
 # trans
 tools_trans_SOURCES = \
 	tools/trans.c \
-	src/internal.c \
 	tools/intra_fit_tools.c \
 	tools/vidinput.c \
 	tools/y4m_input.c \
@@ -434,7 +425,6 @@ tools_trans_LDADD = $(THEORA_LIBS) $(OGG_LIBS) $(PNG_LIBS) $(LIBM)
 # trans_gain
 tools_trans_gain_SOURCES = \
 	tools/trans_gain.c \
-	src/internal.c \
 	tools/vidinput.c \
 	tools/y4m_input.c \
 	tools/od_filter.c \
@@ -448,7 +438,6 @@ tools_trans_gain_LDADD = $(THEORA_LIBS) $(OGG_LIBS) $(PNG_LIBS) $(LIBM)
 #trans2d
 tools_trans2d_SOURCES = \
 	tools/trans2d.c \
-	src/internal.c \
 	tools/intra_fit_tools.c \
 	tools/vidinput.c \
 	tools/y4m_input.c \
@@ -481,8 +470,6 @@ tools_init_intra_xform_SOURCES = \
 	src/filter.c \
 	$(src_dct_SOURCES) \
 	src/intra.c \
-	src/tf.c \
-	src/internal.c \
 	src/intradata.c
 tools_init_intra_xform_CFLAGS = $(THEORA_CFLAGS) $(OGG_CFLAGS) $(PNG_CFLAGS)
 tools_init_intra_xform_LDADD = $(THEORA_LIBS) $(OGG_LIBS) $(PNG_LIBS) $(LIBM)
@@ -539,6 +526,7 @@ endif # ENABLE_TOOLS
 if ENABLE_UNIT_TESTS
 noinst_PROGRAMS += \
 	src/tests/dcttest \
+	src/tests/test_32x32 \
 	src/tests/ectest \
 	src/tests/test_coef_coder \
 	src/tests/logging_test \
@@ -548,6 +536,7 @@ noinst_PROGRAMS += \
 
 TESTS = \
 	src/tests/dcttest \
+	src/tests/test_32x32 \
 	src/tests/ectest \
 	src/tests/test_coef_coder \
 	src/tests/logging_test \
@@ -555,10 +544,14 @@ TESTS = \
 	src/tests/test_filters \
 	src/tests/check_tests
 
-src_tests_dcttest_SOURCES = $(src_dct_SOURCES) src/filter.c src/internal.c
+src_tests_dcttest_SOURCES = $(src_dct_SOURCES) src/filter.c
 src_tests_dcttest_CFLAGS = $(OGG_CFLAGS) \
  -DOD_DCT_CHECK_OVERFLOW -DOD_DCT_TEST -DOD_ENABLE_ASSERTIONS
 src_tests_dcttest_LDADD = $(LIBM)
+
+src_tests_test_32x32_SOURCES = src/tests/test_32x32.c
+src_tests_test_32x32_CFLAGS = $(OGG_CFLAGS) -D_GNU_SOURCE
+src_tests_test_32x32_LDADD = $(LIBM)
 
 src_tests_ectest_SOURCES = src/tests/ectest.c
 src_tests_ectest_CFLAGS = $(OGG_CFLAGS)

configure.ac
@@ -241,6 +241,13 @@ AS_IF([test "$enable_dump_recons" = "yes"], [
   AC_DEFINE([OD_DUMP_RECONS], [1], [Enable reconstructed video dumping])
 ])
 
+AC_ARG_ENABLE([dump-coeffs],
+  AS_HELP_STRING([--enable-dump-coeffs], [Dump transform coefficients]),,
+  [enable_dump_coeffs=no])
+AS_IF([test "$enable_dump_coeffs" = "yes"], [
+  AC_DEFINE([OD_DUMP_COEFFS], [1], [Enable transform coefficient dumping])
+])
+
 AC_ARG_ENABLE([accounting],
   AS_HELP_STRING([--enable-accounting], [Enable bit accounting]),
   [if test "$host_mingw" = "true"; then AC_MSG_ERROR([accounting not supported on Windows]); fi],
@@ -276,6 +283,7 @@ AC_MSG_NOTICE([
     Assembly optimizations ....... ${enable_asm}
     Image dumping ................ ${enable_dump_images}
     Reconstructed video dumping .. ${enable_dump_recons}
+    Transform coeffs dumping ..... ${enable_dump_coeffs}
     Check encoder ................ ${enable_encoder_check}
     Bit accounting ............... ${enable_accounting}
     Entropy coder accounting ..... ${enable_ec_accounting}

src/dct.c
@@ -26,7 +26,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 # include "config.h"
 #endif
 
+#include "block_size.h"
 #include "dct.h"
+#include "tf.h"
 
 /*Adjustments for the quantization step in Q15, a factor of 2^(1/4) per
  octave due to the 3/4 power in the quantizer.*/
@@ -105,13 +107,15 @@ const unsigned char *OD_DCT_ZIGS[OD_NBSIZES + 1] = {
 const od_dct_func_2d OD_FDCT_2D_C[OD_NBSIZES + 1] = {
   od_bin_fdct4x4,
   od_bin_fdct8x8,
-  od_bin_fdct16x16
+  od_bin_fdct16x16,
+  od_bin_fxform32x32
 };
 
 const od_dct_func_2d OD_IDCT_2D_C[OD_NBSIZES + 1] = {
   od_bin_idct4x4,
   od_bin_idct8x8,
-  od_bin_idct16x16
+  od_bin_idct16x16,
+  od_bin_ixform32x32
 };
 
 const od_fdct_func_1d OD_FDCT_1D[OD_NBSIZES + 1] = {
@@ -840,6 +844,38 @@ void od_bin_idct16x16(od_coeff *x, int xstride,
   int i;
   for (i = 0; i < 16; i++) od_bin_idct16(z + i, 16, y + ystride*i);
   for (i = 0; i < 16; i++) od_bin_idct16(x + i, xstride, z + 16*i);
+}
+
+void od_bin_fxform32x32(od_coeff *y, int ystride,
+ const od_coeff *x, int xstride) {
+  od_coeff t[32*32];
+  od_coeff z[16*16];
+  int i;
+  int j;
+  int rc;
+  for (j = 0; j < 2; j++) for (i = 0; i < 2; i++) {
+    for (rc = 0; rc < 16; rc++)
+     od_bin_fdct16(z + 16*rc, x + 16*j*xstride + 16*i + rc, xstride);
+    for (rc = 0; rc < 16; rc++)
+     od_bin_fdct16(t + 16*j*32 + 16*i + 32*rc, z + rc, 16);
+  }
+  od_tf_up_hv(y, ystride, t, 32, 16);
+}
+
+void od_bin_ixform32x32(od_coeff *x, int xstride,
+ const od_coeff *y, int ystride) {
+  od_coeff t[32*32];
+  od_coeff z[16*16];
+  int i;
+  int j;
+  int rc;
+  od_tf_down_hv(t, 32, y, ystride, 32);
+  for (j = 0; j < 2; j++) for (i = 0; i < 2; i++) {
+    for (rc = 0; rc < 16; rc++)
+     od_bin_idct16(z + rc, 16, t + 16*j*32 + 16*i + 32*rc);
+    for (rc = 0; rc < 16; rc++)
+     od_bin_idct16(x + 16*j*xstride + 16*i + rc, xstride, z + 16*rc);
+  }
 }
 
 #if defined(OD_CHECKASM)
@@ -1788,7 +1824,7 @@ static void check_transform(int bszi) {
 
 void run_test(void) {
   int bszi;
-  for (bszi = 0; bszi < OD_NBSIZES; bszi++) check_transform(bszi);
+  for (bszi = 0; bszi < OD_BLOCK_32X32; bszi++) check_transform(bszi);
 }
 
 int main(void) {

src/dct.h
@@ -155,6 +155,11 @@ void od_bin_fdct16x16(od_coeff *y, int ystride,
 void od_bin_idct16x16(od_coeff *x, int xstride,
  const od_coeff *y, int ystride);
 
+void od_bin_fxform32x32(od_coeff *y, int ystride,
+ const od_coeff *x, int xstride);
+void od_bin_ixform32x32(od_coeff *x, int xstride,
+ const od_coeff *y, int ystride);
+
 # if defined(OD_CHECKASM)
 void od_dct_check(int ln, const od_coeff *ref, const od_coeff *x,
  int xstride);

src/decode.c
@@ -149,7 +149,7 @@ static void od_decode_compute_pred(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, od_co
   int x;
   int y;
   int zzi;
-  OD_ASSERT(ln >= 0 && ln <= 2);
+  OD_ASSERT(ln >= 0 && ln <= 3);
   n = 1 << (ln + 2);
   n2 = n*n;
   xdec = dec->state.io_imgs[OD_FRAME_INPUT].planes[pli].xdec;
@@ -172,7 +172,9 @@ static void od_decode_compute_pred(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, od_co
         int m_u;
         int mode;
         od_coeff *coeffs[4];
+#if !OD_DISABLE_INTRA || OD_DISABLE_HAAR_DC
         int strides[4];
+#endif
         /*Calculate the intra-prediction.*/
         coeffs[0] = tf + ((by - (1 << ln)) << 2)*w + ((bx - (1 << ln)) << 2);
         coeffs[1] = tf + ((by - (1 << ln)) << 2)*w + ((bx - (0 << ln)) << 2);
@@ -181,10 +183,12 @@ static void od_decode_compute_pred(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, od_co
         if (!has_ur) {
           coeffs[2] = coeffs[1];
         }
+#if !OD_DISABLE_INTRA || OD_DISABLE_HAAR_DC
         strides[0] = w;
         strides[1] = w;
         strides[2] = w;
         strides[3] = w;
+#endif
         m_l = modes[by*(w >> 2) + bx - 1];
         m_ul = modes[(by - 1)*(w >> 2) + bx - 1];
         m_u = modes[(by - 1)*(w >> 2) + bx];
@@ -195,7 +199,9 @@ static void od_decode_compute_pred(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, od_co
 #else
         mode = od_ec_decode_cdf_unscaled(&dec->ec, mode_cdf, OD_INTRA_NMODES);
 #endif
+#if !OD_DISABLE_INTRA || OD_DISABLE_HAAR_DC
         (*OD_INTRA_GET[ln])(pred, coeffs, strides, mode);
+#endif
 #if OD_DISABLE_INTRA
         OD_CLEAR(pred+1, n2-1);
 #endif
@@ -306,12 +312,12 @@ static void od_block_decode(daala_dec_ctx *dec, od_mb_dec_ctx *ctx, int ln,
   od_coeff *tf;
   od_coeff *md;
   od_coeff *mc;
-  od_coeff pred[16*16];
-  od_coeff predt[16*16];
+  od_coeff pred[OD_BSIZE_MAX*OD_BSIZE_MAX];
+  od_coeff predt[OD_BSIZE_MAX*OD_BSIZE_MAX];
   int lossless;
   int quant;
   int dc_quant;
-  OD_ASSERT(ln >= 0 && ln <= 2);
+  OD_ASSERT(ln >= 0 && ln <= 3);
   n = 1 << (ln + 2);
   lossless = (dec->quantizer[pli] == 0);
   bx <<= ln;

src/encode.c
@@ -341,6 +341,7 @@ struct od_mb_enc_ctx {
 };
 typedef struct od_mb_enc_ctx od_mb_enc_ctx;
 
+#if !defined(OD_DUMP_COEFFS)
 static void od_encode_compute_pred(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, od_coeff *pred,
   int ln, int pli, int bx, int by, int has_ur) {
   int n;
@@ -357,7 +358,7 @@ static void od_encode_compute_pred(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, od_co
   int x;
   int y;
   int zzi;
-  OD_ASSERT(ln >= 0 && ln <= 2);
+  OD_ASSERT(ln >= 0 && ln <= 3);
   n = 1 << (ln + 2);
   n2 = n*n;
   xdec = enc->state.io_imgs[OD_FRAME_INPUT].planes[pli].xdec;
@@ -375,13 +376,17 @@ static void od_encode_compute_pred(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, od_co
     if (bx > 0 && by > 0) {
       if (pli == 0 || OD_DISABLE_CFL) {
         ogg_uint16_t mode_cdf[OD_INTRA_NMODES];
+#if !OD_DISABLE_INTRA
         ogg_uint32_t mode_dist[OD_INTRA_NMODES];
+#endif
         int m_l;
         int m_ul;
         int m_u;
         int mode;
         od_coeff *coeffs[4];
+#if !OD_DISABLE_INTRA || OD_DISABLE_HAAR_DC
         int strides[4];
+#endif
         /*Search predictors from the surrounding blocks.*/
         coeffs[0] = tf + ((by - (1 << ln)) << 2)*w + ((bx - (1 << ln)) << 2);
         coeffs[1] = tf + ((by - (1 << ln)) << 2)*w + ((bx - (0 << ln)) << 2);
@@ -390,27 +395,30 @@ static void od_encode_compute_pred(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, od_co
         if (!has_ur) {
           coeffs[2] = coeffs[1];
         }
+#if !OD_DISABLE_INTRA || OD_DISABLE_HAAR_DC
         strides[0] = w;
         strides[1] = w;
         strides[2] = w;
         strides[3] = w;
+#endif
         m_l = modes[by*(w >> 2) + bx - 1];
         m_ul = modes[(by - 1)*(w >> 2) + bx - 1];
         m_u = modes[(by - 1)*(w >> 2) + bx];
         od_intra_pred_cdf(mode_cdf, enc->state.adapt.mode_probs[pli],
          OD_INTRA_NMODES, m_l, m_ul, m_u);
-        (*OD_INTRA_DIST[ln])(mode_dist, d + (by << 2)*w + (bx << 2), w,
-         coeffs, strides);
-        /*Lambda = 1*/
 #if OD_DISABLE_INTRA
         mode = 0;
 #else
+        (*OD_INTRA_DIST[ln])(mode_dist, d + (by << 2)*w + (bx << 2), w,
+         coeffs, strides);
         /* Make lambda proportional to quantization step size, with exact
            factor based on quick experiments with subset1 (can be improved). */
         mode = od_intra_pred_search(mode_cdf, mode_dist, OD_INTRA_NMODES,
          OD_MINI(32767, enc->quantizer[pli] << 4));
 #endif
+#if !OD_DISABLE_INTRA || OD_DISABLE_HAAR_DC
         (*OD_INTRA_GET[ln])(pred, coeffs, strides, mode);
+#endif
 #if OD_DISABLE_INTRA
         OD_CLEAR(pred+1, n2-1);
 #endif
@@ -539,18 +547,18 @@ static void od_block_encode(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, int ln,
   od_coeff *tf;
   od_coeff *md;
   od_coeff *mc;
-  od_coeff pred[16*16];
-  od_coeff predt[16*16];
-  od_coeff cblock[16*16];
-  od_coeff scalar_out[16*16];
+  od_coeff pred[OD_BSIZE_MAX*OD_BSIZE_MAX];
+  od_coeff predt[OD_BSIZE_MAX*OD_BSIZE_MAX];
+  od_coeff cblock[OD_BSIZE_MAX*OD_BSIZE_MAX];
+  od_coeff scalar_out[OD_BSIZE_MAX*OD_BSIZE_MAX];
   int quant;
   int dc_quant;
   int lossless;
 #if defined(OD_OUTPUT_PRED)
-  od_coeff preds[16*16];
+  od_coeff preds[OD_BSIZE_MAX*OD_BSIZE_MAX];
   int zzi;
 #endif
-  OD_ASSERT(ln >= 0 && ln <= 2);
+  OD_ASSERT(ln >= 0 && ln <= 3);
   n = 1 << (ln + 2);
   bx <<= ln;
   by <<= ln;
@@ -647,6 +655,7 @@ static void od_block_encode(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, int ln,
   (*enc->state.opt_vtbl.idct_2d[ln])(c + (by << 2)*w + (bx << 2), w, preds, n);
 #endif
 }
+#endif
 
 static void od_compute_dcts(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, int pli,
   int bx, int by, int l, int xdec, int ydec) {
@@ -666,6 +675,19 @@ static void od_compute_dcts(daala_enc_ctx *enc, od_mb_enc_ctx *ctx, int pli,
     d -= xdec;
     (*enc->state.opt_vtbl.fdct_2d[d])(c + (by << (2 + d))*w + (bx << (2 + d)), w,
       ctx->c + (by << (2 + d))*w + (bx << (2 + d)), w);
+#if defined(OD_DUMP_COEFFS)
+    {
+      int i;
+      int j;
+      int n;
+      n = 1 << (OD_LOG_BSIZE0 + l);
+      printf("%d ", n);
+      for (j = 0; j < n; j++) for (i = 0; i < n; i++) {
+        printf("%d ", c[j*w + i]);
+      }
+      printf("\n");
+    }
+#endif
   }
   else {
     l--;
@@ -801,6 +823,7 @@ static void od_quantize_haar_dc(daala_enc_ctx *enc, od_mb_enc_ctx *ctx,
 }
 #endif
 
+#if !defined(OD_DUMP_COEFFS)
 static void od_encode_recursive(daala_enc_ctx *enc, od_mb_enc_ctx *ctx,
  int pli, int bx, int by, int l, int xdec, int ydec, int has_ur) {
   int od;
@@ -836,6 +859,7 @@ static void od_encode_recursive(daala_enc_ctx *enc, od_mb_enc_ctx *ctx,
     od_encode_recursive(enc, ctx, pli, bx + 1, by + 1, l, xdec, ydec, 0);
   }
 }
+#endif
 
 static void od_encode_mv(daala_enc_ctx *enc, od_mv_grid_pt *mvg, int vx,
  int vy, int level, int mv_res, int width, int height) {
@@ -1314,8 +1338,10 @@ static void od_encode_residual(daala_enc_ctx *enc, od_mb_enc_ctx *mbctx) {
           od_quantize_haar_dc(enc, mbctx, pli, sbx, sby, 3, xdec, ydec, 0,
            0, sby > 0 && sbx < nhsb - 1);
         }
+#if !defined(OD_DUMP_COEFFS)
         od_encode_recursive(enc, mbctx, pli, sbx, sby, 3, xdec, ydec,
          sby > 0 && sbx < nhsb - 1);
+#endif
       }
         OD_ACCT_UPDATE(&enc->acct, od_ec_enc_tell_frac(&enc->ec),
          OD_ACCT_CAT_PLANE, OD_ACCT_PLANE_UNKNOWN);

commit 608d90117ff950cce0e21c710fedeeb55a3d4963

Fix typo in pvq.c
This fixes an issue that was introduced in commit
f1a9307c8752883e8692476de72a0e1f9665ba30, "Add a 32x32 transform (and
inverse)."

Because of this bug, changing OD_DISABLE_QM to 1 in internal.h doesn't work.

Signed-off-by: Jean-Marc Valin <jmvalin@jmvalin.ca>

src/pvq.c
@@ -87,7 +87,7 @@ static const double OD_PVQ_BETA32_LUMA[10] = {1.5, 1.5, 1.5, 1.5, 1.5, 1.5,
 static const int OD_PVQ_QM4_CB_Q4[2] = {8, 16};
 static const int OD_PVQ_QM8_CB_Q4[5] = {8, 16, 16, 16, 16};
 static const int OD_PVQ_QM16_CB_Q4[8] = {8, 16, 16, 16, 16, 16, 16, 16};
-static const int OD_PVQ_QM32_CB_q4[11] = {8, 16, 16, 16, 16, 16, 16, 16,
+static const int OD_PVQ_QM32_CB_Q4[11] = {8, 16, 16, 16, 16, 16, 16, 16,
  16, 16, 16};
 
 #define OD_PVQ_QM4_CR_Q4 OD_PVQ_QM4_CB_Q4