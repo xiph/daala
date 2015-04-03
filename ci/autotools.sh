fb11f5a47cd6c9c4ebae38b442cde483ec5a4681
Thomas Daede <daede003@umn.edu>
2015-04-03 14:22:18 -0700

    Remove custom libogg path to match Unix makefile build script.

--- ci/autotools.sh
+++ ci/autotools.sh
@@ -2,13 +2,12 @@
 # continuous integration test script
 # run this from the top-level source directory
 
-OGG_PATH=/srv/jenkins/jobs/libogg/workspace
 VIDEOS=/usr/local/share/videos
 
 ./autogen.sh
-CFLAGS='-O2 -g' ./configure --enable-assertions --enable-check-asm --enable-logging --enable-accounting PKG_CONFIG_PATH=${OGG_PATH}
+CFLAGS='-O2 -g' ./configure --enable-assertions --enable-check-asm --enable-logging --enable-accounting
 make clean
-make distcheck PKG_CONFIG_PATH=${OGG_PATH}
+make distcheck
 make docs
 make
 ./examples/encoder_example -k 4 ${VIDEOS}/claire_qcif-2frames.y4m -o out.$$.ogv
