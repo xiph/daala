8cf864134ec105a5a3936f0f7625830e87192384
Tristan Matthews <tmatth@videolan.org>
2015-06-01 17:01:14 -0400

    ci: add script to validate lossless encode/decode

---
+++ ci/lossless.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+# continuous integration test script
+# run this from the top-level source directory
+
+VIDEOS=/usr/local/share/videos
+
+./autogen.sh
+CFLAGS='-O2 -g' ./configure --enable-assertions --enable-check-asm --enable-logging --enable-accounting
+make clean all
+DAALA_ROOT=. tools/check_lossless.sh ${VIDEOS}/claire_qcif-2frames.y4m
