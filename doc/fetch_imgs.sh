43744cf7f8b3bb7bef525a8016587b75a37f1732
Jean-Marc Valin <jmvalin@jmvalin.ca>
2015-07-05 23:13:07 -0400

    Some documentation on intra paint/deringing

---
+++ doc/fetch_imgs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget http://people.xiph.org/~jm/daala/paint_demo/fruits_block2_luma.jpg
+wget http://people.xiph.org/~jm/daala/paint_demo/fruits_direction_small.png
+wget http://people.xiph.org/~jm/daala/paint_demo/fruits_edges2_luma_small.png
+wget http://people.xiph.org/~jm/daala/paint_demo/fruits_recons2_luma.jpg
