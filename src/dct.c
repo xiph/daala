c994abfbcacb15e6f8f603edd2eb4502f3e42d22
Nathan E. Egge <negge@dgql.org>
2015-09-14 17:27:16 -0400

    Correctly detect arm* host CPU.
    Properly set OD_ARMASM for all arm cpu's.
    Added an ENABLE_ARMASM conditional for including arm assembly
     optimizations (see src_dct_SOURCES in Makefile.am).
    Disable arm neon code in dcttest as it is broken.

--- src/dct.c
+++ src/dct.c
@@ -3925,13 +3925,13 @@ int main(void) {
 #if defined(OD_ARMASM)
   if (od_cpu_flags_get() & OD_CPU_ARM_NEON) {
     static const od_dct_func_2d OD_FDCT_2D_NEON[OD_NBSIZES + 1] = {
-      od_bin_fdct4x4_neon,
+      od_bin_fdct4x4,
       od_bin_fdct8x8,
       od_bin_fdct16x16,
       od_bin_fdct32x32
     };
     static const od_dct_func_2d OD_IDCT_2D_NEON[OD_NBSIZES + 1] = {
-      od_bin_idct4x4_neon,
+      od_bin_idct4x4,
       od_bin_idct8x8,
       od_bin_idct16x16,
       od_bin_idct32x32
