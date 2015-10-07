xiph/daala

commit dd740e68d49f9028bbc3c856f3528ec3a3eb4a65

Fix to 983 patch; use xstride in input image plane increment
Yushin caught an error in encode input_img setup; planes were allocated properly, but weren't
being set up taking the xstride into account.

src/encode.c
@@ -242,16 +242,16 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
     plane_buf_width = frame_buf_width >> info->plane_info[pli].xdec;
     plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
     iplane = img->planes + pli;
-    iplane->data = input_img_data
-     + (OD_BUFFER_PADDING >> info->plane_info[pli].xdec)
-     + plane_buf_width*(OD_BUFFER_PADDING >> info->plane_info[pli].ydec);
-    input_img_data += plane_buf_width*plane_buf_height;
     iplane->xdec = info->plane_info[pli].xdec;
     iplane->ydec = info->plane_info[pli].ydec;
     iplane->bitdepth = reference_bits;
     /*Internal buffers are always planar.*/
     iplane->xstride = reference_bytes;
-    iplane->ystride = plane_buf_width;
+    iplane->ystride = plane_buf_width*iplane->xstride;
+    iplane->data = input_img_data
+     + iplane->xstride*(OD_BUFFER_PADDING >> info->plane_info[pli].xdec)
+     + iplane->ystride*(OD_BUFFER_PADDING >> info->plane_info[pli].ydec);
+    input_img_data += plane_buf_width*iplane->ystride;
   }
 #if defined(OD_DUMP_IMAGES)
   /*Fill in the line buffers.*/

commit b5a5ad45ed67de30f8d5097b3101be3b20c0a935

encode: fix out-of-bounds write
Increment by plane_buf_height*plane_buf_width*xstride instead of
plane_buf_width*plane_buf_width*xstride.

Regression from dd740e68d49f9028bbc3c856f3528ec3a3eb4a65

src/encode.c
@@ -251,7 +251,7 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
     iplane->data = input_img_data
      + iplane->xstride*(OD_BUFFER_PADDING >> info->plane_info[pli].xdec)
      + iplane->ystride*(OD_BUFFER_PADDING >> info->plane_info[pli].ydec);
-    input_img_data += plane_buf_width*iplane->ystride;
+    input_img_data += plane_buf_height*iplane->ystride;
   }
 #if defined(OD_DUMP_IMAGES)
   /*Fill in the line buffers.*/