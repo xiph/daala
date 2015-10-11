e33259c7bec015e47c401ae42a188075bb3a31cc
Tristan Matthews <tmatth@videolan.org>
2015-10-11 18:19:49 -0400

    pvq: sync parameter names in .c and .h

--- src/pvq_code.h
+++ src/pvq_code.h
@@ -42,13 +42,12 @@ void od_encode_quantizer_scaling(daala_enc_ctx *enc, int q_scaling, int bx,
  int by, int skip);
 #endif
 
-int od_pvq_encode(daala_enc_ctx *enc, od_coeff *predt, od_coeff *cblock,
-                   od_coeff *scalar_out, int scale, int pli, int bs,
-                   const double *beta, int robust, int is_keyframe,
-                   int q_scaling, int bx, int by);
+int od_pvq_encode(daala_enc_ctx *enc, od_coeff *ref, od_coeff *in,
+ od_coeff *out, int q0, int pli, int bs, const double *beta, int robust,
+ int is_keyframe, int q_scaling, int bx, int by);
 
-void od_pvq_decode(daala_dec_ctx *dec, od_coeff *ref, od_coeff *out, int scale,
-                   int pli, int bs, const double *beta, int robust,
-                   int is_keyframe, unsigned int *flags, int block_skip);
+void od_pvq_decode(daala_dec_ctx *dec, od_coeff *ref, od_coeff *out, int q0,
+ int pli, int bs, const double *beta, int robust, int is_keyframe,
+ unsigned int *flags, int block_skip);
 
 #endif
