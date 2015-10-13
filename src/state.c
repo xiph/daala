xiph/daala

commit d135749ef00743b63207ae3e682f1912cfad3440

Extend od_img to high-depth support, add parsing to encoder_example
This is a slightly fleshed out version of TD's Issue 312 patch.  It
adds the needed fields to od_img for high-depth support, adds
depth-aware allocation to state, encode and decode, and high-depth
parsing/reading to the encoder_example (though right now high-depth is
merely truncated to 8 bit during encode).

This patch breaks both ABI and header format.

src/state.c
@@ -122,11 +122,17 @@ static int od_state_ref_imgs_init(od_state *state, int nrefs) {
   int frame_buf_height;
   int plane_buf_width;
   int plane_buf_height;
+  int reference_bytes;
+  int reference_bits;
   int imgi;
   int pli;
   OD_ASSERT(nrefs == 4);
   info = &state->info;
   data_sz = 0;
+  /*Reference bit depth is constant over the lifetime of od_state, and by
+    extension, an encode or decode ctx.*/
+  reference_bytes = state->full_precision_references ? 2 : 1;
+  reference_bits = state->full_precision_references ? 8 + OD_COEFF_SHIFT : 8;
   /*TODO: Check for overflow before allocating.*/
   frame_buf_width = state->frame_width + (OD_BUFFER_PADDING << 1);
   frame_buf_height = state->frame_height + (OD_BUFFER_PADDING << 1);
@@ -134,7 +140,7 @@ static int od_state_ref_imgs_init(od_state *state, int nrefs) {
     /*Reserve space for this plane in nrefs reference images.*/
     plane_buf_width = frame_buf_width >> info->plane_info[pli].xdec;
     plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
-    data_sz += plane_buf_width*plane_buf_height*nrefs;
+    data_sz += plane_buf_width*plane_buf_height*reference_bytes*nrefs;
   }
   state->ref_img_data = ref_img_data =
     (unsigned char *)od_aligned_malloc(data_sz, 32);
@@ -148,17 +154,18 @@ static int od_state_ref_imgs_init(od_state *state, int nrefs) {
     img->width = state->frame_width;
     img->height = state->frame_height;
     for (pli = 0; pli < img->nplanes; pli++) {
-      plane_buf_width = frame_buf_width >> info->plane_info[pli].xdec;
-      plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
       iplane = img->planes + pli;
-      iplane->data = ref_img_data
-       + (OD_BUFFER_PADDING >> info->plane_info[pli].xdec)
-       + plane_buf_width*(OD_BUFFER_PADDING >> info->plane_info[pli].ydec);
-      ref_img_data += plane_buf_width*plane_buf_height;
       iplane->xdec = info->plane_info[pli].xdec;
       iplane->ydec = info->plane_info[pli].ydec;
-      iplane->xstride = 1;
-      iplane->ystride = plane_buf_width;
+      plane_buf_width = frame_buf_width >> iplane->xdec;
+      plane_buf_height = frame_buf_height >> iplane->ydec;
+      iplane->bitdepth = reference_bits;
+      iplane->xstride = reference_bytes;
+      iplane->ystride = plane_buf_width*reference_bytes;
+      iplane->data = ref_img_data
+       + (OD_BUFFER_PADDING >> iplane->xdec)*iplane->xstride
+       + (OD_BUFFER_PADDING >> iplane->ydec)*iplane->ystride;
+      ref_img_data += plane_buf_height*iplane->ystride;
     }
   }
   /*Mark all of the reference image buffers available.*/
@@ -182,11 +189,22 @@ void od_restore_fpu(od_state *state) {
 }
 
 void od_state_opt_vtbl_init_c(od_state *state) {
-  state->opt_vtbl.mc_predict1fmv = od_mc_predict1fmv8_c;
-  state->opt_vtbl.mc_blend_full = od_mc_blend_full8_c;
-  state->opt_vtbl.mc_blend_full_split = od_mc_blend_full_split8_c;
-  state->opt_vtbl.mc_blend_multi = od_mc_blend_multi8_c;
-  state->opt_vtbl.mc_blend_multi_split = od_mc_blend_multi_split8_c;
+  if (state->full_precision_references) {
+    OD_ASSERT(0);
+    /*Incoming:
+      state->opt_vtbl.mc_predict1fmv = od_mc_predict1fmv16_c;
+      state->opt_vtbl.mc_blend_full = od_mc_blend_full16_c;
+      state->opt_vtbl.mc_blend_full_split = od_mc_blend_full_split16_c;
+      state->opt_vtbl.mc_blend_multi = od_mc_blend_multi16_c;
+      state->opt_vtbl.mc_blend_multi_split = od_mc_blend_multi_split16_c;*/
+  }
+  else {
+    state->opt_vtbl.mc_predict1fmv = od_mc_predict1fmv8_c;
+    state->opt_vtbl.mc_blend_full = od_mc_blend_full8_c;
+    state->opt_vtbl.mc_blend_full_split = od_mc_blend_full_split8_c;
+    state->opt_vtbl.mc_blend_full = od_mc_blend_multi8_c;
+    state->opt_vtbl.mc_blend_full_split = od_mc_blend_multi_split8_c;
+  }
   state->opt_vtbl.restore_fpu = od_restore_fpu_c;
   OD_COPY(state->opt_vtbl.fdct_2d, OD_FDCT_2D_C, OD_NBSIZES + 1);
   OD_COPY(state->opt_vtbl.idct_2d, OD_IDCT_2D_C, OD_NBSIZES + 1);
@@ -209,6 +227,11 @@ static int od_state_init_impl(od_state *state, const daala_info *info) {
   if (nplanes <= 0 || nplanes > OD_NPLANES_MAX) return OD_EINVAL;
   /*The first plane (the luma plane) must not be subsampled.*/
   if (info->plane_info[0].xdec || info->plane_info[0].ydec) return OD_EINVAL;
+  /*The bitdepth is restricted to a few allowed values.*/
+  if (info->bitdepth_mode < OD_BITDEPTH_MODE_8
+   || info->bitdepth_mode > OD_BITDEPTH_MODE_12) {
+    return OD_EINVAL;
+  }
   OD_CLEAR(state, 1);
   OD_COPY(&state->info, info, 1);
   /*Frame size is a multiple of a super block.*/
@@ -218,6 +241,10 @@ static int od_state_init_impl(od_state *state, const daala_info *info) {
    ~(OD_BSIZE_MAX - 1);
   state->nhmvbs = state->frame_width >> OD_LOG_MVBSIZE_MIN;
   state->nvmvbs = state->frame_height >> OD_LOG_MVBSIZE_MIN;
+  /*The master switch; once FPR is ready to go, this can be set to always-on,
+     or on only when encoding high-depth.
+    FPR must be on for lossless high-depth.*/
+  state->full_precision_references = 0;
   od_state_opt_vtbl_init(state);
   if (OD_UNLIKELY(od_state_ref_imgs_init(state, 4))) {
     return OD_EFAULT;
@@ -585,8 +612,8 @@ void od_state_pred_block(od_state *state,
     od_state_pred_block(state, buf + half_yblk_sz*ystride,
      ystride, xstride, pli, vx, vy + half_mvb_sz, log_mvb_sz - 1);
     od_state_pred_block(state,
-     buf + half_yblk_sz*ystride + half_xblk_sz*xstride,
-     ystride, xstride, pli, vx + half_mvb_sz, vy + half_mvb_sz, log_mvb_sz - 1);
+     buf + half_yblk_sz*ystride + half_xblk_sz*xstride, ystride, xstride,
+     pli, vx + half_mvb_sz, vy + half_mvb_sz, log_mvb_sz - 1);
   }
   else {
     int oc;

examples/encoder_example.c
@@ -60,7 +60,21 @@ struct av_input{
   daala_plane_info video_plane_info[OD_NPLANES_MAX];
   od_img video_img;
   int video_cur_img;
+  int video_depth;
+  int video_swapendian;
 };
+
+#define SWAP(a, b)  do {a ^= b; b ^= a; a ^= b;} while(0)
+
+static int host_is_big_endian() {
+  union {
+    uint16_t pattern;
+    unsigned char bytewise[2];
+  } m;
+  m.pattern = (uint16_t)0xdead; /* beef */
+  if (m.bytewise[0] == 0xde) return 1;
+  return 0;
+}
 
 static void daala_to_ogg_packet(ogg_packet *op, daala_packet *dp) {
   op->packet     = dp->packet;
@@ -198,8 +212,46 @@ static void id_y4m_file(av_input *avin, const char *file, FILE *test) {
   /*TODO: Specify chroma offsets.*/
   avin->video_plane_info[0].xdec = 0;
   avin->video_plane_info[0].ydec = 0;
+  avin->video_depth = 8;
+  avin->video_swapendian = 0;
   if (strcmp(avin->video_chroma_type, "444") == 0) {
     avin->video_nplanes = 3;
+    avin->video_plane_info[1].xdec = 0;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 0;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "444p10") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 10;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 0;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 0;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "444p12") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 12;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 0;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 0;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "444p14") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 14;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 0;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 0;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "444p16") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 16;
+    avin->video_swapendian = host_is_big_endian();
     avin->video_plane_info[1].xdec = 0;
     avin->video_plane_info[1].ydec = 0;
     avin->video_plane_info[2].xdec = 0;
@@ -216,6 +268,42 @@ static void id_y4m_file(av_input *avin, const char *file, FILE *test) {
   }
   else if (strcmp(avin->video_chroma_type, "422") == 0) {
     avin->video_nplanes = 3;
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "422p10") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 10;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "422p12") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 12;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "422p14") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 14;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 0;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 0;
+  }
+  else if (strcmp(avin->video_chroma_type, "422p16") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 16;
+    avin->video_swapendian = host_is_big_endian();
     avin->video_plane_info[1].xdec = 1;
     avin->video_plane_info[1].ydec = 0;
     avin->video_plane_info[2].xdec = 1;
@@ -238,6 +326,42 @@ static void id_y4m_file(av_input *avin, const char *file, FILE *test) {
     avin->video_plane_info[2].xdec = 1;
     avin->video_plane_info[2].ydec = 1;
   }
+  else if (strcmp(avin->video_chroma_type, "420p10") == 0){
+    avin->video_nplanes = 3;
+    avin->video_depth = 10;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 1;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 1;
+  }
+  else if (strcmp(avin->video_chroma_type, "420p12") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 12;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 1;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 1;
+  }
+  else if (strcmp(avin->video_chroma_type, "420p14") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 14;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 1;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 1;
+  }
+  else if (strcmp(avin->video_chroma_type, "420p16") == 0) {
+    avin->video_nplanes = 3;
+    avin->video_depth = 16;
+    avin->video_swapendian = host_is_big_endian();
+    avin->video_plane_info[1].xdec = 1;
+    avin->video_plane_info[1].ydec = 1;
+    avin->video_plane_info[2].xdec = 1;
+    avin->video_plane_info[2].ydec = 1;
+  }
   else if (strcmp(avin->video_chroma_type, "mono") == 0) {
     avin->video_nplanes = 1;
   }
@@ -255,11 +379,16 @@ static void id_y4m_file(av_input *avin, const char *file, FILE *test) {
     iplane = img->planes + pli;
     iplane->xdec = avin->video_plane_info[pli].xdec;
     iplane->ydec = avin->video_plane_info[pli].ydec;
-    iplane->xstride = 1;
-    iplane->ystride = (avin->video_pic_w
-     + (1 << iplane->xdec) - 1)  >>  iplane->xdec;
-    iplane->data = (unsigned char *)_ogg_malloc(iplane->ystride*
-     ((avin->video_pic_h + (1 << iplane->ydec) - 1)  >>  iplane->ydec));
+    /*At this moment, Y4M input is always planar.*/
+    /*The input depth need not match the encoded depth.
+      The input copy will convert to whatever was set for
+       di.bitdepth_mode.*/
+    iplane->xstride = avin->video_depth > 8 ? 2 : 1;
+    iplane->ystride = ((avin->video_pic_w
+     + (1<<iplane->xdec)-1)>>iplane->xdec)*iplane->xstride;
+    iplane->bitdepth = avin->video_depth;
+    iplane->data = _ogg_malloc(iplane->ystride*
+     ((avin->video_pic_h + (1<<iplane->ydec) - 1)>>iplane->ydec));
   }
 }
 
@@ -331,17 +460,23 @@ int fetch_and_process_video(av_input *avin, ogg_page *page,
       img = &avin->video_img;
       for (pli = 0; pli < img->nplanes; pli++) {
         od_img_plane *iplane;
+        int bytes;
         size_t plane_sz;
         iplane = img->planes + pli;
+        bytes = iplane->bitdepth > 8 ? 2 : 1;
         plane_sz = ((avin->video_pic_w + (1 << iplane->xdec) - 1)
          >> iplane->xdec)*((avin->video_pic_h + (1 << iplane->ydec)
-         - 1) >> iplane->ydec);
-        ret = fread(iplane->data/* + (avin->video_pic_y >> iplane->ydec)
-         *iplane->ystride + (avin->video_picx >> iplane->xdec)*/, 1, plane_sz,
-         avin->video_infile);
+         - 1) >> iplane->ydec)*bytes;
+        ret = fread(iplane->data, 1, plane_sz, avin->video_infile);
         if (ret != plane_sz) {
           fprintf(stderr, "Error reading YUV frame data.\n");
           exit(1);
+        }
+        if (bytes == 2 && avin->video_swapendian) {
+          size_t i;
+          for (i = 0; i < plane_sz; i += 2) {
+            SWAP(iplane->data[i], iplane->data[i + 1]);
+          }
         }
       }
       if (skip && (*skip) > 0) {
@@ -689,6 +824,30 @@ int main(int argc, char **argv) {
   daala_info_init(&di);
   di.pic_width = avin.video_pic_w;
   di.pic_height = avin.video_pic_h;
+  switch (avin.video_depth) {
+    case 8: {
+      di.bitdepth_mode = OD_BITDEPTH_MODE_8;
+      break;
+    }
+    case 10: {
+      di.bitdepth_mode = OD_BITDEPTH_MODE_10;
+      break;
+    }
+    case 14:
+    case 16: {
+      fprintf(stderr, "Daala natively supports only 8, 10 and 12 bit depth\n"
+       "Input will be truncated to 12 bit depth for encoding\b\n");
+    }
+    /* Fall through */
+    case 12: {
+      di.bitdepth_mode = OD_BITDEPTH_MODE_12;
+      break;
+    }
+    default: {
+      fprintf(stderr, "Unsupported input bit depth (%d)\n", avin.video_depth);
+      exit(1);
+    }
+  }
   di.timebase_numerator = avin.video_fps_n;
   di.timebase_denominator = avin.video_fps_d;
   di.frame_duration = 1;

include/daala/codec.h
@@ -144,18 +144,31 @@ int daala_log_init(void);
 
 /** Representation of a single component within an image or frame. */
 struct od_img_plane {
+  /** Image data is stored as an unsigned octet type whether it's
+      actually 8 bit or a multi-byte depth. */
   unsigned char *data;
-  /** The decimation factor in x direction. Pixels are reduced by a factor of
-      2^xdec so 0 is none, 1 is decimated by a factor of 2. ( YUV420 will
-      have xdec of 1 and ydec also of 1. YUV444 will have xdec and ydec set to
-      zero ). */
+  /** The decimation factor in the x and y direction. Pixels are reduced
+      by a factor of 2^xdec so 0 is none, 1 is decimated by a factor of 2.
+      ( YUV420 will  have xdec of 1 and ydec also of 1. YUV444 will have
+      xdec and ydec set to zero ). */
   unsigned char xdec;
   unsigned char ydec;
-  /** Distance in memory between two pixels horizontally next to each other in
-      (is always 1 in encoder). */
+  /** Distance in memory between two pixels horizontally next to each other.
+      The value is in bytes regardless of the 'actual' underlying depth
+      (either unsigned bytes for 8 bit video or unsigned 16 bit shorts for
+      high-depth video). The xstride may be larger than the actual data
+      width calculated from the bitdepth; this implies packed rather than
+      planar data. */
   int xstride;
-  /** Distance in memory between two pixels vertically next to each other. */
+  /** Distance in memory between two pixels vertically next to each other.
+      As with xstride, this value is always in bytes. */
   int ystride;
+  /** 8 for 'normal' video precision; data is unsigned bytes centered on 128.
+      Greater-than-8 indicates high-depth video; data is unnormalized
+      host-endian order unsigned signed 16-bit shorts (two octets).
+      For example, 10 bit video would declare a bit depth of 10, use the
+      lower 10 bits of each 16 bit short, and center on 512. */
+  int bitdepth;
 };
 
 /** Representation of an image or video frame. */
@@ -176,6 +189,17 @@ struct daala_plane_info {
   unsigned char ydec;
 };
 
+/**\name Bit Depths
+ * The three video bit depths currently supported by Daala.*/
+/*@{*/
+/**8-bit mode.*/
+#define OD_BITDEPTH_MODE_8 (1)
+/**10-bit mode.*/
+#define OD_BITDEPTH_MODE_10 (2)
+/**12-bit mode.*/
+#define OD_BITDEPTH_MODE_12 (3)
+/*@}*/
+
 /** Configuration parameters for a codec instance. */
 struct daala_info {
   unsigned char version_major;
@@ -190,6 +214,9 @@ struct daala_info {
   uint32_t timebase_denominator;
   uint32_t frame_duration;
   int keyframe_granule_shift;
+  /** bitdepth_mode is one of the three OD_BITDEPTH_MODE_X choices allowed
+   * above. */
+  int bitdepth_mode;
   int nplanes;
   daala_plane_info plane_info[OD_NPLANES_MAX];
    /** key frame rate defined how often a key frame is emitted by encoder in

src/decode.c
@@ -56,6 +56,8 @@ static int od_dec_init(od_dec_ctx *dec, const daala_info *info,
   int frame_buf_height;
   int plane_buf_width;
   int plane_buf_height;
+  int output_bytes;
+  int output_bits;
   int ret;
   int pli;
   (void)setup;
@@ -67,20 +69,21 @@ static int od_dec_init(od_dec_ctx *dec, const daala_info *info,
   dec->user_mv_grid = NULL;
   dec->user_mc_img = NULL;
   data_sz = 0;
+  output_bits = 8 + (info->bitdepth_mode - OD_BITDEPTH_MODE_8)*2;
+  output_bytes = output_bits > 8 ? 2 : 1;
   /*TODO: Check for overflow before allocating.*/
   frame_buf_width = dec->state.frame_width + (OD_BUFFER_PADDING << 1);
   frame_buf_height = dec->state.frame_height + (OD_BUFFER_PADDING << 1);
   for (pli = 0; pli < info->nplanes; pli++) {
     plane_buf_width = frame_buf_width >> info->plane_info[pli].xdec;
     plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
-    data_sz += plane_buf_width*plane_buf_height;
+    data_sz += plane_buf_width*plane_buf_height*output_bytes;
   }
   dec->output_img_data = output_img_data =
     (unsigned char *)od_aligned_malloc(data_sz, 32);
   if (OD_UNLIKELY(!dec->output_img_data)) {
     return OD_EFAULT;
   }
-  /*8-bit only for now.*/
   img = &dec->output_img;
   img->nplanes = info->nplanes;
   img->width = dec->state.frame_width;
@@ -89,14 +92,16 @@ static int od_dec_init(od_dec_ctx *dec, const daala_info *info,
     plane_buf_width = frame_buf_width >> info->plane_info[pli].xdec;
     plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
     iplane = img->planes + pli;
-    iplane->data = output_img_data
-     + (OD_BUFFER_PADDING >> info->plane_info[pli].xdec)
-     + plane_buf_width*(OD_BUFFER_PADDING >> info->plane_info[pli].ydec);
-    output_img_data += plane_buf_width*plane_buf_height;
     iplane->xdec = info->plane_info[pli].xdec;
     iplane->ydec = info->plane_info[pli].ydec;
-    iplane->xstride = 1;
-    iplane->ystride = plane_buf_width;
+    iplane->bitdepth = output_bits;
+    /*At this moment, our output is always planar.*/
+    iplane->xstride = output_bytes;
+    iplane->ystride = plane_buf_width*iplane->xstride;
+    iplane->data = output_img_data
+      + iplane->xstride*(OD_BUFFER_PADDING >> info->plane_info[pli].xdec)
+      + iplane->ystride*(OD_BUFFER_PADDING >> info->plane_info[pli].ydec);
+    output_img_data += plane_buf_height*iplane->ystride;
   }
 #if OD_ACCOUNTING
   od_accounting_init(&dec->acct);

src/encode.c
@@ -177,6 +177,8 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
   int frame_buf_height;
   int plane_buf_width;
   int plane_buf_height;
+  int reference_bytes;
+  int reference_bits;
   int ret;
 #if defined(OD_DUMP_BSIZE_DIST)
   char dist_fname[1024];
@@ -204,6 +206,9 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
   enc->params.mv_level_max = 4;
   enc->bs = (od_block_size_comp *)malloc(sizeof(*enc->bs));
   data_sz = 0;
+  reference_bytes = enc->state.full_precision_references ? 2 : 1;
+  reference_bits =
+   enc->state.full_precision_references ? 8 + OD_COEFF_SHIFT : 8;
   /*TODO: Check for overflow before allocating.*/
   frame_buf_width = enc->state.frame_width + (OD_BUFFER_PADDING << 1);
   frame_buf_height = enc->state.frame_height + (OD_BUFFER_PADDING << 1);
@@ -211,7 +216,7 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
     plane_buf_width = frame_buf_width >> info->plane_info[pli].xdec;
     plane_buf_height = frame_buf_height >> info->plane_info[pli].ydec;
     /*Reserve space for input plane buffer */
-    data_sz += plane_buf_width*plane_buf_height;
+    data_sz += plane_buf_width*plane_buf_height*reference_bytes;
 #if defined(OD_DUMP_IMAGES)
     /*Reserve space for this plane in 1 visualization image.*/
     data_sz += plane_buf_width*plane_buf_height << 2;
@@ -228,8 +233,7 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
   if (OD_UNLIKELY(!input_img_data)) {
     return OD_EFAULT;
   }
-  /*Fill in the input img structure.
-    8-bit only for now.*/
+  /*Fill in the input img structure.*/
   img = &enc->input_img;
   img->nplanes = info->nplanes;
   img->width = enc->state.frame_width;
@@ -244,7 +248,9 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
     input_img_data += plane_buf_width*plane_buf_height;
     iplane->xdec = info->plane_info[pli].xdec;
     iplane->ydec = info->plane_info[pli].ydec;
-    iplane->xstride = 1;
+    iplane->bitdepth = reference_bits;
+    /*Internal buffers are always planar.*/
+    iplane->xstride = reference_bytes;
     iplane->ystride = plane_buf_width;
   }
 #if defined(OD_DUMP_IMAGES)
@@ -269,6 +275,7 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
     input_img_data += plane_buf_width*plane_buf_height;
     iplane->xdec = info->plane_info[pli].xdec;
     iplane->ydec = info->plane_info[pli].ydec;
+    iplane->bitdepth = 8;
     iplane->xstride = 1;
     iplane->ystride = plane_buf_width;
   }
@@ -285,6 +292,7 @@ static int od_enc_init(od_enc_ctx *enc, const daala_info *info) {
     input_img_data += plane_buf_width*plane_buf_height;
     iplane->xdec = info->plane_info[pli].xdec;
     iplane->ydec = info->plane_info[pli].ydec;
+    iplane->bitdepth = 8;
     iplane->xstride = 1;
     iplane->ystride = plane_buf_width;
   }
@@ -517,8 +525,28 @@ static void od_img_plane_copy_pad8(od_img_plane *dst_p,
     src_data = src_p->data;
     dst = dst_data;
     for (y = 0; y < pic_height; y++) {
-      if (sxstride == 1) OD_COPY(dst, src_data, pic_width);
-      else for (x = 0; x < pic_width; x++) dst[x] = *(src_data + sxstride*x);
+      if (sxstride == 1) {
+        OD_COPY(dst, src_data, pic_width);
+      }
+      else {
+        if (src_p->bitdepth <= 8) {
+          /*1 byte per pixel*/
+          for (x = 0; x < pic_width; x++) {
+            dst[x] = *(src_data + sxstride*x);
+          }
+        }
+        else {
+          /*2 bytes per pixel*/
+          /*This is effectively a temporary placeholder until next commit when
+             we add a smart copy.
+            For now, just don't crash or produce gibberish;
+             accept the high-depth input and truncate it. */
+          for (x = 0; x < pic_width; x++) {
+            dst[x] = *(src_data + sxstride*x) +
+             (*(src_data + sxstride*x + 1) << 8) >> src_p->bitdepth - 8;
+          }
+        }
+      }
       dst += dstride;
       src_data += systride;
     }

src/infodec.c
@@ -175,6 +175,11 @@ int daala_decode_header_in(daala_info *info,
       tmpi = oggbyte_read1(&obb);
       if (tmpi < 0 || tmpi >= 32) return OD_EBADHEADER;
       info->keyframe_granule_shift = tmpi;
+      info->bitdepth_mode = oggbyte_read1(&obb);
+      if (info->bitdepth_mode < OD_BITDEPTH_MODE_8
+       || info->bitdepth_mode > OD_BITDEPTH_MODE_12) {
+        return OD_EBADHEADER;
+      }
       info->nplanes = oggbyte_read1(&obb);
       if ((info->nplanes < 1) || (info->nplanes > OD_NPLANES_MAX)) {
         return OD_EBADHEADER;

src/infoenc.c
@@ -54,6 +54,9 @@ int daala_encode_flush_header(daala_enc_ctx *_enc, daala_comment *_dc,
       oggbyte_write4(&_enc->obb, info->frame_duration);
       OD_ASSERT(info->keyframe_granule_shift < 32);
       oggbyte_write1(&_enc->obb, info->keyframe_granule_shift);
+      OD_ASSERT(info->bitdepth_mode >= OD_BITDEPTH_MODE_8
+       && info->bitdepth_mode <= OD_BITDEPTH_MODE_12);
+      oggbyte_write1(&_enc->obb, info->bitdepth_mode);
       OD_ASSERT((info->nplanes >= 1) && (info->nplanes <= OD_NPLANES_MAX));
       oggbyte_write1(&_enc->obb, info->nplanes);
       for (pli = 0; pli < info->nplanes; ++pli) {

src/state.h
@@ -179,7 +179,8 @@ struct od_state{
   uint32_t        cpu_flags;
   int32_t         frame_width;
   int32_t         frame_height;
-  /** Buffer for the 4 ref images. */
+  int             full_precision_references;
+  /** Buffer for the reference images. */
   int                 ref_imgi[OD_FRAME_MAX+1];
   /** Pointers to the ref images so one can move them around without coping
       them. */

commit 26d05f0b78cf39ebd64cbce96aad17f017832412

Fix crash with assembly off introduced in d135749.
The non-multiresolution blend functions were initialized with the
multiresolution functions, causing crashes if these were not later
replaced with optimized functions. This occurs when the platform
does not have optimized routines (ARM) or assembly is turned off.

src/state.c
@@ -202,8 +202,8 @@ void od_state_opt_vtbl_init_c(od_state *state) {
     state->opt_vtbl.mc_predict1fmv = od_mc_predict1fmv8_c;
     state->opt_vtbl.mc_blend_full = od_mc_blend_full8_c;
     state->opt_vtbl.mc_blend_full_split = od_mc_blend_full_split8_c;
-    state->opt_vtbl.mc_blend_full = od_mc_blend_multi8_c;
-    state->opt_vtbl.mc_blend_full_split = od_mc_blend_multi_split8_c;
+    state->opt_vtbl.mc_blend_multi = od_mc_blend_multi8_c;
+    state->opt_vtbl.mc_blend_multi_split = od_mc_blend_multi_split8_c;
   }
   state->opt_vtbl.restore_fpu = od_restore_fpu_c;
   OD_COPY(state->opt_vtbl.fdct_2d, OD_FDCT_2D_C, OD_NBSIZES + 1);