5abddb2ef8c7d10f258bc3a391551adaa7d71d99
Tristan Matthews <tmatth@videolan.org>
2015-12-01 19:56:43 -0500

    sse2mc: fix out-of-bounds pointer arithmetic

--- src/x86/sse2mc.c
+++ src/x86/sse2mc.c
@@ -425,17 +425,17 @@ void od_mc_predict1fmv8_sse2(od_state *state, unsigned char *dst,
           __m128i row01_hi;
           __m128i out;
           OD_ASSERT((buff_p + i + ((0 - OD_SUBPEL_TOP_APRON_SZ)*xblk_sz) + 15)
-           < buff + sizeof(buff));
+           < buff + sizeof(buff)/sizeof(buff[0]));
           OD_ASSERT((buff_p + i + ((1 - OD_SUBPEL_TOP_APRON_SZ)*xblk_sz) + 15)
-           < buff + sizeof(buff));
+           < buff + sizeof(buff)/sizeof(buff[0]));
           OD_ASSERT((buff_p + i + ((2 - OD_SUBPEL_TOP_APRON_SZ)*xblk_sz) + 15)
-           < buff + sizeof(buff));
+           < buff + sizeof(buff)/sizeof(buff[0]));
           OD_ASSERT((buff_p + i + ((3 - OD_SUBPEL_TOP_APRON_SZ)*xblk_sz) + 15)
-           < buff + sizeof(buff));
+           < buff + sizeof(buff)/sizeof(buff[0]));
           OD_ASSERT((buff_p + i + ((4 - OD_SUBPEL_TOP_APRON_SZ)*xblk_sz) + 15)
-           < buff + sizeof(buff));
+           < buff + sizeof(buff)/sizeof(buff[0]));
           OD_ASSERT((buff_p + i + ((5 - OD_SUBPEL_TOP_APRON_SZ)*xblk_sz) + 15)
-           < buff + sizeof(buff));
+           < buff + sizeof(buff)/sizeof(buff[0]));
           /*Load input coeffs from each row, 8 shorts at one time.*/
           row0_src = _mm_loadu_si128((__m128i *)
            (buff_p + i + ((0 - OD_SUBPEL_TOP_APRON_SZ)*(1 << log_xblk_sz))));
