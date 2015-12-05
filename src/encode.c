72894b02ba7d5a60e53856e78c8fed9cbaa77a50
Jean-Marc Valin <jmvalin@jmvalin.ca>
2015-12-04 23:51:20 -0500

    Boosting keyframes by 3 instead of 2
    Still no sign of keyframe popping being worse than 1.
    
    ntt-short1
              LOW (%)  MEDIUM (%) HIGH (%)
        PSNR -4.464021 -4.288482 -3.579677
     PSNRHVS -4.314016 -3.184823 -1.136055
        SSIM -4.583961 -4.062950 -2.740593
    FASTSSIM -4.976522 -4.643331 -3.457730

--- src/encode.c
+++ src/encode.c
@@ -2798,7 +2798,7 @@ int daala_encode_img_in(daala_enc_ctx *enc, od_img *img, int duration) {
     if ((mbctx.is_keyframe || mbctx.is_golden_frame)
      && enc->state.coded_quantizer[pli] != 0) {
       enc->state.coded_quantizer[pli] =
-       OD_MAXI(1, enc->state.coded_quantizer[pli] - 2);
+       OD_MAXI(1, enc->state.coded_quantizer[pli] - 3);
       enc->state.quantizer[pli] =
        od_codedquantizer_to_quantizer(enc->state.coded_quantizer[pli]);
     }
