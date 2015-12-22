38b661aa7399b1278315caab27ede14fba3d8be7
Tristan Matthews <tmatth@videolan.org>
2015-12-22 15:19:32 -0500

    add parentheses to OD_PLANE_SZ macro

--- src/internal.h
+++ src/internal.h
@@ -124,7 +124,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
    in Q4 format.*/
 # define OD_QUALITY_SHIFT (4)
 # define OD_LOSSLESS(_ctx, _pli) (_ctx->state.quantizer[_pli] == 0)
-# define OD_PLANE_SZ(_dim, _dec) ((_dim + (1 << _dec) - 1) >> _dec)
+# define OD_PLANE_SZ(_dim, _dec) (((_dim) + (1 << (_dec)) - 1) >> (_dec))
 
 # if defined(OD_ENABLE_ASSERTIONS)
 #  if OD_GNUC_PREREQ(2, 5, 0)
