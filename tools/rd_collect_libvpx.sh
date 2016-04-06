1345a885d0fdf32d915c62f68d9b8170dcd2cec3
Nathan E. Egge <negge@dgql.org>
2016-04-06 13:07:05 -0400

    Use ivf format with libvpx codecs.

--- tools/rd_collect_libvpx.sh
+++ tools/rd_collect_libvpx.sh
@@ -19,9 +19,9 @@ RANGE=$(seq 1 63)
 QSTR="-y --min-q=\$x --max-q=\$x"
 
 for x in $RANGE; do
-  $VPXENC --codec=$CODEC --good --cpu-used=0 $(echo $QSTR | sed 's/\$x/'$x'/g') -o $BASENAME.vpx $FILE 2> $BASENAME-$x-enc.out
-  $VPXDEC --codec=$CODEC -o $BASENAME.y4m $BASENAME.vpx
-  SIZE=$(wc -c $BASENAME.vpx | awk '{ print $1 }')
+  $VPXENC --codec=$CODEC --good --cpu-used=0 --ivf $(echo $QSTR | sed 's/\$x/'$x'/g') -o $BASENAME.ivf $FILE 2> $BASENAME-$x-enc.out
+  $VPXDEC --codec=$CODEC -o $BASENAME.y4m $BASENAME.ivf
+  SIZE=$(wc -c $BASENAME.ivf | awk '{ print $1 }')
   $DUMP_PSNR $FILE $BASENAME.y4m > $BASENAME-$x-psnr.out 2> /dev/null
   FRAMES=$(cat $BASENAME-$x-psnr.out | grep ^0 | wc -l)
   PIXELS=$(($WIDTH*$HEIGHT*$FRAMES))
@@ -29,7 +29,7 @@ for x in $RANGE; do
   PSNRHVS=$($DUMP_PSNRHVS $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
   SSIM=$($DUMP_SSIM $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
   FASTSSIM=$($DUMP_FASTSSIM -c $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  rm $BASENAME.vpx $BASENAME.y4m $BASENAME-$x-enc.out $BASENAME-$x-psnr.out
+  rm $BASENAME.ivf $BASENAME.y4m $BASENAME-$x-enc.out $BASENAME-$x-psnr.out
   echo $x $PIXELS $SIZE $PSNR $PSNRHVS $SSIM $FASTSSIM >> $BASENAME.out
   #tail -1 $BASENAME.out
 done
