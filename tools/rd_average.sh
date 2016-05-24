863d189a1b8aff28c5f31ca00e0177fbb1e17321
Tristan Matthews <tmatth@videolan.org>
2016-05-24 12:47:06 -0400

    rd_average: error out if output file already exists

--- tools/rd_average.sh
+++ tools/rd_average.sh
@@ -11,4 +11,9 @@ if [ -n "$OUTPUT" ]; then
   TOTAL="$OUTPUT.out"
 fi
 
+if [ -e "$TOTAL" ]; then
+  echo "ERROR: $TOTAL already exists and will be included in average, please remove it first"
+  exit 1
+fi
+
 awk '{size[$1]+=$2;bytes[$1]+=$3;psnr[$1]+=$2*$4;psnrhvs[$1]+=$2*$5;ssim[$1]+=$2*$6;fastssim[$1]+=$2*$7;}END{for(i in size)print i,size[i],bytes[i],psnr[i]/size[i],psnrhvs[i]/size[i],ssim[i]/size[i],fastssim[i]/size[i];}' $@ | sort -n > $TOTAL
