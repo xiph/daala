8acee57da16ae15b16c9a53878fb9f51ab4e296a
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-06-14 16:41:36 -0400

    First PVQ optimization: using the od_pvq_rate() approximation on RDO pass
    No change in metrics by default, but when enabled:
    PSNR YCbCr:      0.44%     -0.05%      0.17%
       PSNRHVS:      0.53%
          SSIM:      0.47%
        MSSSIM:      0.45%
     CIEDE2000:      0.34%

--- src/pvq_encoder.h
+++ src/pvq_encoder.h
@@ -46,6 +46,6 @@ void od_encode_quantizer_scaling(daala_enc_ctx *enc, int q_scaling, int bx,
 int od_pvq_encode(daala_enc_ctx *enc, od_coeff *ref, const od_coeff *in,
  od_coeff *out, int q0, int pli, int bs, const double *beta, int robust,
  int is_keyframe, int q_scaling, int bx, int by, const int16_t *qm,
- const int16_t *qm_inv);
+ const int16_t *qm_inv, int speed);
 
 #endif
