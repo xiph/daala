fab08c71346634acb444045f1bc1077bbb3693ef
Thomas Daede <daede003@umn.edu>
2016-06-24 16:25:57 -0700

    Add ci script to fetch all test videos.

---
+++ ci/fetch-videos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+wget -c https://people.xiph.org/~tdaede/claire_qcif-2frames.y4m
+wget -c https://people.xiph.org/~tdaede/tos444.y4m
+wget -c https://people.xiph.org/~unlord/nes420.y4m
