xiph/daala

commit a9d834a103bb1903faf098d2078988d6693f58aa

pvq: fixed-point od_apply_householder
objective-fast1:
PSNR YCbCr:      0.01%     -0.02%      0.22%
   PSNRHVS:      0.07%
      SSIM:      0.06%
    MSSSIM:      0.03%
 CIEDE2000:      0.00%

subset1:
        LOW (%)  MEDIUM (%) HIGH (%)
    PSNR -0.005594  0.002910  0.010791
 PSNRHVS -0.002126  0.004317  0.052591
    SSIM -0.009023  0.002315  0.006972
FASTSSIM -0.008637 -0.022084 -0.015729

src/pvq.c
@@ -483,6 +483,15 @@ int od_compute_householder(od_val16 *r, int n, od_val32 gr, int *sign,
   return m;
 }
 
+#define OD_RCP_INSHIFT 15
+#define OD_RCP_OUTSHIFT 14
+static od_val16 od_rcp(od_val16 x)
+{
+  /*FIXME: replace with int approximation.*/
+  return OD_MINI(32767, (od_val32)floor(.5
+   + (1 << OD_RCP_OUTSHIFT)*(1./(x/(double)(1 << OD_RCP_INSHIFT)))));
+}
+
 /** Applies Householder reflection from compute_householder(). The
  * reflection is its own inverse.
  *
@@ -495,8 +504,17 @@ void od_apply_householder(od_val16 *out, const od_val16 *x, const od_val16 *r,
  int n) {
   int i;
   od_val32 proj;
-  double proj_1;
+  od_val16 proj_1;
   od_val32 l2r;
+#if !defined(OD_FLOAT_PVQ)
+  od_val16 proj_norm;
+  od_val16 l2r_norm;
+  od_val16 rcp;
+  int proj_shift;
+  int l2r_shift;
+  int outshift;
+#endif
+  /*FIXME: Can we get l2r and/or l2r_shift from an earlier computation?*/
   l2r = 0;
   for (i = 0; i < n; i++) {
     l2r += OD_MULT16_16(r[i], r[i]);
@@ -506,10 +524,45 @@ void od_apply_householder(od_val16 *out, const od_val16 *x, const od_val16 *r,
   for (i = 0; i < n; i++) {
     proj += OD_MULT16_16(r[i], x[i]);
   }
+#if defined(OD_FLOAT_PVQ)
   proj_1 = proj*2./(1e-100 + l2r);
   for (i = 0; i < n; i++) {
-    out[i] = OD_ROUND16(x[i] - r[i]*proj_1);
+    out[i] = x[i] - r[i]*proj_1;
   }
+#else
+  /*l2r_norm is [0.5, 1.0[ in Q15.*/
+  l2r_shift = (OD_ILOG(l2r) - 1) - 14;
+  l2r_norm = OD_VSHR_ROUND(l2r, l2r_shift);
+  rcp = od_rcp(l2r_norm);
+  proj_shift = (OD_ILOG(abs(proj)) - 1) - 14;
+  /*proj_norm is [0.5, 1.0[ in Q15.*/
+  proj_norm = OD_VSHR_ROUND(proj, proj_shift);
+  proj_1 = OD_MULT16_16_Q15(proj_norm, rcp);
+  /*The proj*2. in the float code becomes -1 in the final outshift.
+    The sign of l2r_shift is positive since we're taking the reciprocal of
+     l2r_norm and this is a right shift.*/
+  outshift = OD_MINI(30, OD_RCP_OUTSHIFT - proj_shift - 1 + l2r_shift);
+  if (outshift >= 0) {
+    for (i = 0; i < n; i++) {
+      int32_t tmp;
+      tmp = OD_MULT16_16(r[i], proj_1);
+      tmp = OD_SHR_ROUND(tmp, outshift);
+      out[i] = x[i] - tmp;
+    }
+  }
+  else {
+    /*FIXME: Can we make this case impossible?
+      Right now, if r[] is all zeros except for 1, 2, or 3 ones, and
+       if x[] is all zeros except for large values at the same position as the
+       ones in r[], then we can end up with a shift of -1.*/
+    for (i = 0; i < n; i++) {
+      int32_t tmp;
+      tmp = OD_MULT16_16(r[i], proj_1);
+      tmp = OD_SHL(tmp, -outshift);
+      out[i] = x[i] - tmp;
+    }
+  }
+#endif
 }
 
 #if !defined(OD_FLOAT_PVQ)

commit 12d28af77ea70e57989d1211dfca4ff7de7a0e3b

pvq: fix build for OD_FLOAT_PVQ
Regression from a9d834a103bb1903faf098d2078988d6693f58aa

src/pvq.c
@@ -483,6 +483,7 @@ int od_compute_householder(od_val16 *r, int n, od_val32 gr, int *sign,
   return m;
 }
 
+#if !defined(OD_FLOAT_PVQ)
 #define OD_RCP_INSHIFT 15
 #define OD_RCP_OUTSHIFT 14
 static od_val16 od_rcp(od_val16 x)
@@ -491,6 +492,7 @@ static od_val16 od_rcp(od_val16 x)
   return OD_MINI(32767, (od_val32)floor(.5
    + (1 << OD_RCP_OUTSHIFT)*(1./(x/(double)(1 << OD_RCP_INSHIFT)))));
 }
+#endif
 
 /** Applies Householder reflection from compute_householder(). The
  * reflection is its own inverse.