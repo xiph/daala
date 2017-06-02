ec6da88e41b51ea69af9f09f6c4e50ca67b65b25
Nathan E. Egge <negge@dgql.org>
2017-06-02 12:01:33 -0400

    Add quotes $@ in rd_collect.sh script.

--- tools/rd_collect.sh
+++ tools/rd_collect.sh
@@ -389,4 +389,4 @@ if [ -z "$CORES" ]; then
   #echo "CORES not set, using $CORES"
 fi
 
-find -L $@ -type f -name "*.y4m" -print0 | xargs -0 -n1 -P$CORES $RD_COLLECT_SUB
+find -L "$@" -type f -name "*.y4m" -print0 | xargs -0 -n1 -P$CORES $RD_COLLECT_SUB
