46c3777df9a9efe9c03c34ba0d6ad13d8e195e86
Justin Nickelsburg <nickelsburg99@gmail.com>
2019-07-09 16:34:47 -0400

    Add QP parallelization to rd_collect for mozjpeg.

--- tools/rd_collect.sh
+++ tools/rd_collect.sh
@@ -258,6 +258,10 @@ case $CODEC in
       exit 1
     fi
 
+    if [ -z "$QPS" ]; then
+      QPS=$(seq 100 -1 0)
+    fi
+
     export RD_COLLECT_SUB=$(dirname $0)/rd_collect_jpeg.sh
     ;;
   theora)
@@ -513,7 +517,7 @@ if [ -z "$CORES" ]; then
 fi
 
 case $CODEC in
-  libaom | libaom-rt | daala | rav1e | svt-av1 | vp8 | vp9 | x264 | x265 | jpeg)
+  libaom | libaom-rt | daala | rav1e | svt-av1 | vp8 | vp9 | x264 | x265 | jpeg | mozjpeg)
     FILES=$(find -L "$@" -type f -name "*.y4m")
     for f in $FILES; do for q in $QPS; do printf "%s\0" $f $q; done; done | xargs -0 -n2 -P$CORES $RD_COLLECT_SUB
     for f in $FILES; do cat $(basename $f)-$CODEC-*.out | sort -n > $(basename $f)-$CODEC.out && rm $(basename $f)-$CODEC-*.out; done
