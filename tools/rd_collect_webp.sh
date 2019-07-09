a69853e436d3ff6bd48b599df8cfd4ad07eedf42
Justin Nickelsburg <nickelsburg99@gmail.com>
2019-07-09 17:21:59 -0400

    Add QP parallelization to rd_collect for webp.

--- tools/rd_collect_webp.sh
+++ tools/rd_collect_webp.sh
@@ -7,8 +7,9 @@ if [ -z $RD_COLLECT_SUB ]; then
 fi
 
 FILE=$1
+QP=$2
 
-BASENAME=$(basename $FILE)
+BASENAME=$(basename $FILE)-$CODEC-$QP
 rm $BASENAME.out 2> /dev/null || true
 echo $BASENAME
 tail -n+3 $FILE > $BASENAME-in.yuv
@@ -17,19 +18,15 @@ WIDTH=$(head -1 $FILE | cut -d\  -f 2 | tr -d 'W')
 HEIGHT=$(head -1 $FILE | cut -d\  -f 3 | tr -d 'H')
 PIXELS=$(($WIDTH*$HEIGHT))
 
-for x in $(seq 100 -1 0); do
-  $CWEBP -q $x -s $WIDTH $HEIGHT $BASENAME-in.yuv -o $BASENAME.webp 2> /dev/null
-  $DWEBP $BASENAME.webp -yuv -o $BASENAME.yuv 2> /dev/null
-  $YUV2YUV4MPEG $BASENAME -w$WIDTH -h$HEIGHT -an0 -ad0 -c420mpeg2
-  SIZE=$(stat -c %s $BASENAME.webp)
-  PSNR=$($DUMP_PSNR $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  PSNRHVS=$($DUMP_PSNRHVS $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  SSIM=$($DUMP_SSIM $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  FASTSSIM=$($DUMP_FASTSSIM -c $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  CIEDE=$($DUMP_CIEDE $FILE $BASENAME.y4m 2> /dev/null | grep Total | cut -d' ' -f2-)
-  rm $BASENAME.webp $BASENAME.yuv $BASENAME.y4m
-  echo -$x $PIXELS $SIZE $PSNR $PSNRHVS $SSIM $FASTSSIM $CIEDE >> $BASENAME.out
-  #tail -1 $BASENAME.out
-done
-
-rm $BASENAME-in.yuv
+$CWEBP -q $QP -s $WIDTH $HEIGHT $BASENAME-in.yuv -o $BASENAME.webp 2> /dev/null
+$DWEBP $BASENAME.webp -yuv -o $BASENAME.yuv 2> /dev/null
+$YUV2YUV4MPEG $BASENAME -w$WIDTH -h$HEIGHT -an0 -ad0 -c420mpeg2
+SIZE=$(stat -c %s $BASENAME.webp)
+PSNR=$($DUMP_PSNR $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+PSNRHVS=$($DUMP_PSNRHVS $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+SSIM=$($DUMP_SSIM $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+FASTSSIM=$($DUMP_FASTSSIM -c $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+CIEDE=$($DUMP_CIEDE $FILE $BASENAME.y4m 2> /dev/null | grep Total | cut -d' ' -f2-)
+rm $BASENAME.webp $BASENAME.yuv $BASENAME.y4m $BASENAME-in.yuv
+echo -$QP $PIXELS $SIZE $PSNR $PSNRHVS $SSIM $FASTSSIM $CIEDE >> $BASENAME.out
+#tail -1 $BASENAME.out
