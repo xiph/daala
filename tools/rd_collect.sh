f06181d3af162af983d33b696a38a3d5c513e267
Justin Nickelsburg <nickelsburg99@gmail.com>
2019-07-09 15:21:53 -0400

    Add QP parallelization to rd_collect for libvpx.

--- tools/rd_collect.sh
+++ tools/rd_collect.sh
@@ -123,6 +123,10 @@ case $CODEC in
       exit 1
     fi
 
+    if [ -z "$QPS" ]; then
+      QPS=$(seq 12 5 64)
+    fi
+
     export RD_COLLECT_SUB=$(dirname $0)/rd_collect_libvpx.sh
     ;;
   vp9)
@@ -149,6 +153,10 @@ case $CODEC in
       echo "Executable not found VPXDEC=$VPXDEC"
       echo "Do you have the right LIBVPX_ROOT=$LIBVPX_ROOT"
       exit 1
+    fi
+
+    if [ -z "$QPS" ]; then
+      QPS="20 32 43 55 63"
     fi
 
     export RD_COLLECT_SUB=$(dirname $0)/rd_collect_libvpx.sh
@@ -493,7 +501,7 @@ if [ -z "$CORES" ]; then
 fi
 
 case $CODEC in
-  libaom | libaom-rt | daala | rav1e | svt-av1)
+  libaom | libaom-rt | daala | rav1e | svt-av1 | vp8 | vp9)
     FILES=$(find -L "$@" -type f -name "*.y4m")
     for f in $FILES; do for q in $QPS; do printf "%s\0" $f $q; done; done | xargs -0 -n2 -P$CORES $RD_COLLECT_SUB
     for f in $FILES; do cat $(basename $f)-$CODEC-*.out | sort -n > $(basename $f)-$CODEC.out && rm $(basename $f)-$CODEC-*.out; done
--- tools/rd_collect_libvpx.sh
+++ tools/rd_collect_libvpx.sh
@@ -7,37 +7,35 @@ if [ -z $RD_COLLECT_SUB ]; then
 fi
 
 FILE=$1
+QP=$2
 
-BASENAME=$(basename $FILE)-$CODEC
+BASENAME=$(basename $FILE)-$CODEC-$QP
 rm $BASENAME.out 2> /dev/null || true
 echo $BASENAME
 
 WIDTH=$(head -1 $FILE | cut -d\  -f 2 | tr -d 'W')
 HEIGHT=$(head -1 $FILE | cut -d\  -f 3 | tr -d 'H')
 
-RANGE=$(seq 1 63)
 case $CODEC in
 vp8)
-  QSTR="--end-usage=cq --cq-level=\$x"
+  QSTR="--end-usage=cq --cq-level=$QP"
   ;;
 vp9)
   QSTR="--end-usage=q --cq-level=\$x"
   ;;
 esac
 
-for x in $RANGE; do
-  $VPXENC --codec=$CODEC --good --cpu-used=0 --ivf $(echo $QSTR | sed 's/\$x/'$x'/g') -o $BASENAME.ivf $FILE 2> $BASENAME-$x-enc.out
-  $VPXDEC --codec=$CODEC -o $BASENAME.y4m $BASENAME.ivf
-  SIZE=$(wc -c $BASENAME.ivf | awk '{ print $1 }')
-  $DUMP_PSNR $FILE $BASENAME.y4m > $BASENAME-$x-psnr.out 2> /dev/null
-  FRAMES=$(cat $BASENAME-$x-psnr.out | grep ^0 | wc -l)
-  PIXELS=$(($WIDTH*$HEIGHT*$FRAMES))
-  PSNR=$(cat $BASENAME-$x-psnr.out | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  PSNRHVS=$($DUMP_PSNRHVS $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  SSIM=$($DUMP_SSIM $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  FASTSSIM=$($DUMP_FASTSSIM -c $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
-  CIEDE=$($DUMP_CIEDE $FILE $BASENAME.y4m 2> /dev/null | grep Total | cut -d' ' -f2-)
-  rm $BASENAME.ivf $BASENAME.y4m $BASENAME-$x-enc.out $BASENAME-$x-psnr.out
-  echo $x $PIXELS $SIZE $PSNR $PSNRHVS $SSIM $FASTSSIM $CIEDE >> $BASENAME.out
-  #tail -1 $BASENAME.out
-done
+$VPXENC --good --codec=$CODEC --cpu-used=0 --ivf $(echo $QSTR | sed 's/\$x/'$QP'/g') -o $BASENAME.ivf $FILE 2> $BASENAME-$QP-enc.out
+$VPXDEC --codec=$CODEC -o $BASENAME.y4m $BASENAME.ivf
+SIZE=$(wc -c $BASENAME.ivf | awk '{ print $1 }')
+$DUMP_PSNR $FILE $BASENAME.y4m > $BASENAME-$QP-psnr.out 2> /dev/null
+FRAMES=$(cat $BASENAME-$QP-psnr.out | grep ^0 | wc -l)
+PIXELS=$(($WIDTH*$HEIGHT*$FRAMES))
+PSNR=$(cat $BASENAME-$QP-psnr.out | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+PSNRHVS=$($DUMP_PSNRHVS $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+SSIM=$($DUMP_SSIM $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+FASTSSIM=$($DUMP_FASTSSIM -c $FILE $BASENAME.y4m 2> /dev/null | grep Total | tr -s ' ' | cut -d\  -f $((4+$PLANE*2)))
+CIEDE=$($DUMP_CIEDE $FILE $BASENAME.y4m 2> /dev/null | grep Total | cut -d' ' -f2-)
+rm $BASENAME.ivf $BASENAME.y4m $BASENAME-$QP-enc.out $BASENAME-$QP-psnr.out
+echo $QP $PIXELS $SIZE $PSNR $PSNRHVS $SSIM $FASTSSIM $CIEDE >> $BASENAME.out
+#tail -1 $BASENAME.out
